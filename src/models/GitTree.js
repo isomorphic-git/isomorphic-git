421308a926f053c3ec5b0eb2cf3eb609e3753d04
Will Hilton <wmhilton@gmail.com>
2017-08-12 00:54:52 -0400

    Successfully parse tree objects

--- src/models/GitTree.js
+++ src/models/GitTree.js
@@ -1,6 +1,33 @@
+function parseBuffer (buffer) {
+  let _entries = []
+  let cursor = 0
+  while (cursor < buffer.length) {
+    let space = buffer.indexOf(32, cursor)
+    // TODO: assert space > -1
+    let nullchar = buffer.indexOf(0, cursor)
+    // TODO: assert nullchar > -1
+    let mode = buffer.slice(cursor, space).toString('utf8')
+    if (mode === '40000') mode = '040000' // makes it line up neater in printed output
+    let path = buffer.slice(space + 1, nullchar).toString('utf8')
+    let oid = buffer.slice(nullchar + 1, nullchar + 21).toString('hex')
+    cursor = nullchar + 21
+    _entries.push({mode, path, sha: oid})
+  }
+  return _entries
+}
+
 export default class GitTree {
   constructor (entries) {
-    this._entries = entries
+    if (Buffer.isBuffer(entries)) {
+      this._entries = parseBuffer(entries)
+    } else if (Array.isArray(entries)) {
+      this._entries = entries
+    } else {
+      throw new Error('invalid type passed to GitTree constructor')
+    }
+  }
+  static from (tree) {
+    return new GitTree(tree)
   }
   render () {
     return this._entries.map(entry => `${entry.mode} ${entry.type} ${entry.sha}    ${entry.path}`).join('\n')
