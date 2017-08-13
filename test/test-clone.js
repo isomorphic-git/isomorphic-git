6c4145ed31fa5b40649ce82e15f372673b69e82d
Will Hilton <wmhilton@gmail.com>
2017-08-13 19:01:36 -0400

    Checkout writes files to disk!

--- test/test-clone.js
+++ test/test-clone.js
@@ -4,7 +4,7 @@ import {exists, tmpdir, cleanup} from './_helpers'
 
 test(async t => {
   let dir = await tmpdir()
-  await git(`${dir}`).githubToken(process.env.GITHUB_TOKEN).clone(`https://github.com/wmhilton/nde`)
+  await git(`${dir}`).githubToken(process.env.GITHUB_TOKEN).clone(`https://github.com/wmhilton/esgit`)
   t.true(exists(`${dir}`))
   t.true(exists(`${dir}/.git/objects`))
   t.true(exists(`${dir}/.git/refs/remotes/origin/master`))
