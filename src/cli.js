d0ac842aa94dce58030b9e06090d289ae043699c
Will Hilton <wmhilton@gmail.com>
2017-08-30 20:38:04 -0400

    Add a really basic CLI for testing around with

---
+++ src/cli.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+import minimisted from 'minimisted'
+import git from '.'
+
+minimisted(async function ({
+  _: [command, arg, ...args],
+  ...opts
+}) {
+  const dir = process.cwd()
+  const repo = git(dir)
+  let cmd = `git('${dir}')`
+  for (let key of Object.keys(opts)) {
+    repo[key](opts[key])
+    cmd += `.${key}('${opts[key]}')`
+  }
+  cmd += `.${command}(${arg ? `'${arg}'` : ''})`
+  console.log(cmd)
+  let result = await repo[command](arg)
+  console.log(result)
+})
