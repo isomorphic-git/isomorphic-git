75ffc1cc5fc2097f3a7362079cb5569c51069fc0
Will Hilton <wmhilton@gmail.com>
2017-09-02 22:31:17 -0400

    Fix ava watch by moving fixtures out of test folder

--- test/test-remove.js
+++ test/test-remove.js
@@ -5,23 +5,23 @@ import read from '../lib/utils/read'
 import write from '../lib/utils/write'
 
 test.beforeEach(async t => {
-  await write('test/fixtures/esgit.git/index', await read('test/fixtures/esgit.git/backup.index'))
+  await write('fixtures/esgit.git/index', await read('fixtures/esgit.git/backup.index'))
 })
 
 test.serial('gitIndex.remove(file)', async t => {
-  const repo = git('test/fixtures/esgit.git')
+  const repo = git('fixtures/esgit.git')
   let orig = (await repo.list()).length
   await repo.remove('README.md')
   t.true(orig === (await repo.list()).length + 1)
 })
 
 test.serial('gitIndex.remove(dir)', async t => {
-  const repo = git('test/fixtures/esgit.git')
+  const repo = git('fixtures/esgit.git')
   let orig = (await repo.list()).length
   await repo.remove('test')
   t.true(orig === (await repo.list()).length + 7)
 })
 
 test.afterEach(async t => {
-  await write('test/fixtures/esgit.git/index', await read('test/fixtures/esgit.git/backup.index'))
+  await write('fixtures/esgit.git/index', await read('fixtures/esgit.git/backup.index'))
 })
--- test/test-resolveRef.js
+++ test/test-resolveRef.js
@@ -2,14 +2,14 @@ import test from 'ava'
 import resolveRef from '../lib/utils/resolveRef'
 
 test('1e40fdfba1cf17f3c9f9f3d6b392b1865e5147b9', async t => {
-  let ref = await resolveRef({gitdir: 'test/fixtures/esgit.git', ref: '1e40fdfba1cf17f3c9f9f3d6b392b1865e5147b9'})
+  let ref = await resolveRef({gitdir: 'fixtures/esgit.git', ref: '1e40fdfba1cf17f3c9f9f3d6b392b1865e5147b9'})
   t.snapshot(ref)
 })
 test('test-branch', async t => {
-  let ref = await resolveRef({gitdir: 'test/fixtures/esgit.git', ref: 'origin/test-branch'})
+  let ref = await resolveRef({gitdir: 'fixtures/esgit.git', ref: 'origin/test-branch'})
   t.snapshot(ref)
 })
 test('test-tag', async t => {
-  let ref = await resolveRef({gitdir: 'test/fixtures/esgit.git', ref: 'test-tag'})
+  let ref = await resolveRef({gitdir: 'fixtures/esgit.git', ref: 'test-tag'})
   t.snapshot(ref)
 })
