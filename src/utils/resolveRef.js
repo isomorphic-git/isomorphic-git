cd28eaeb13df483fb9cb6513846fa9d2da785e6e
Will Hilton <wmhilton@gmail.com>
2017-09-02 23:33:26 -0400

    Resolve refs recursively. Handle 'HEAD' ref.

--- src/utils/resolveRef.js
+++ src/utils/resolveRef.js
@@ -3,27 +3,32 @@ import read from './read'
 
 export default async function resolveRef ({gitdir, ref}) {
   let sha
-  // Is it a SHA?
-  if (/^[0-9a-fA-F]+$/.test(ref)) {
-    // Is it a complete SHA (already dereferenced)?
-    if (ref.length === 40) {
-      if (await exists(`${gitdir}/objects/${ref.slice(0,2)}/${ref.slice(2)}`)) {
-        return ref.trim()
-      }
-    // Is it a partial SHA?
-    } else if (ref.length === 7) {
-      // TODO: use file globbing to match partial SHAs
+  // Is it a ref pointer?
+  if (ref.startsWith('ref: ')) {
+    ref = ref.slice('ref: '.length)
+    sha = await read(`${gitdir}/${ref}`, {encoding: 'utf8'})
+    if (sha) return resolveRef({gitdir, ref: sha.trim()})
+  }
+  // Is it a complete and valid SHA?
+  if (ref.length === 40) {
+    if (await exists(`${gitdir}/objects/${ref.slice(0,2)}/${ref.slice(2)}`)) {
+      return ref
     }
+  }
+  // Is it a special ref?
+  if (ref === 'HEAD' || ref === 'MERGE_HEAD') {
+    sha = await read(`${gitdir}/${ref}`, {encoding: 'utf8'})
+    if (sha) return resolveRef({gitdir, ref: sha.trim()})
   }
   // Is it a (local) branch?
   sha = await read(`${gitdir}/refs/heads/${ref}`, {encoding: 'utf8'})
-  if (sha) return sha.trim()
+  if (sha) return resolveRef({gitdir, ref: sha.trim()})
   // Is it a tag?
   sha = await read(`${gitdir}/refs/tags/${ref}`, {encoding: 'utf8'})
-  if (sha) return sha.trim()
+  if (sha) return resolveRef({gitdir, ref: sha.trim()})
   // Is it remote branch?
   sha = await read(`${gitdir}/refs/remotes/${ref}`, {encoding: 'utf8'})
-  if (sha) return sha.trim()
+  if (sha) return resolveRef({gitdir, ref: sha.trim()})
   // Do we give up?
   throw new Error(`Could not resolve reference ${ref}`)
 }
