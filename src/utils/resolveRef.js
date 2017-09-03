40de616c9ccbc26788f0b57b468937b47291aa3a
Will Hilton <wmhilton@gmail.com>
2017-09-03 00:35:21 -0400

    Add commit command

--- src/utils/resolveRef.js
+++ src/utils/resolveRef.js
@@ -1,13 +1,18 @@
 import exists from './exists'
 import read from './read'
 
-export default async function resolveRef ({gitdir, ref}) {
+export default async function resolveRef ({gitdir, ref, depth}) {
+  if (depth !== undefined) {
+    depth--
+    if (depth === -1) {
+      return ref
+    }
+  }
   let sha
   // Is it a ref pointer?
   if (ref.startsWith('ref: ')) {
     ref = ref.slice('ref: '.length)
-    sha = await read(`${gitdir}/${ref}`, {encoding: 'utf8'})
-    if (sha) return resolveRef({gitdir, ref: sha.trim()})
+    return resolveRef({gitdir, ref, depth})
   }
   // Is it a complete and valid SHA?
   if (ref.length === 40) {
@@ -18,17 +23,22 @@ export default async function resolveRef ({gitdir, ref}) {
   // Is it a special ref?
   if (ref === 'HEAD' || ref === 'MERGE_HEAD') {
     sha = await read(`${gitdir}/${ref}`, {encoding: 'utf8'})
-    if (sha) return resolveRef({gitdir, ref: sha.trim()})
+    if (sha) return resolveRef({gitdir, ref: sha.trim(), depth})
+  }
+  // Is it a full ref?
+  if (ref.startsWith('refs/')) {
+    sha = await read(`${gitdir}/${ref}`, {encoding: 'utf8'})
+    if (sha) return resolveRef({gitdir, ref: sha.trim(), depth})
   }
   // Is it a (local) branch?
   sha = await read(`${gitdir}/refs/heads/${ref}`, {encoding: 'utf8'})
-  if (sha) return resolveRef({gitdir, ref: sha.trim()})
+  if (sha) return resolveRef({gitdir, ref: sha.trim(), depth})
   // Is it a tag?
   sha = await read(`${gitdir}/refs/tags/${ref}`, {encoding: 'utf8'})
-  if (sha) return resolveRef({gitdir, ref: sha.trim()})
+  if (sha) return resolveRef({gitdir, ref: sha.trim(), depth})
   // Is it remote branch?
   sha = await read(`${gitdir}/refs/remotes/${ref}`, {encoding: 'utf8'})
-  if (sha) return resolveRef({gitdir, ref: sha.trim()})
+  if (sha) return resolveRef({gitdir, ref: sha.trim(), depth})
   // Do we give up?
   throw new Error(`Could not resolve reference ${ref}`)
 }
