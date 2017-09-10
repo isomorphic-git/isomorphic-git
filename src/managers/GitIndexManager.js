c78fd88901f31679b419e2a59b2af24aed973e74
Will Hilton <wmhilton@gmail.com>
2017-09-10 19:18:22 -0400

    Temporarily knock out the lockfile stuff so esgit works in the browser

--- src/managers/GitIndexManager.js
+++ src/managers/GitIndexManager.js
@@ -1,21 +1,21 @@
 // @flow
-import LockManager from 'travix-lock-manager'
+// import LockManager from 'travix-lock-manager'
 import read from '../utils/read'
 import GitIndex from '../models/GitIndex'
-import Lock from '../utils/lockfile'
+// import Lock from '../utils/lockfile'
 
 // TODO: replace with an LRU cache?
 const map /*: Map<string, GitIndex> */ = new Map()
-const lm = new LockManager()
+// const lm = new LockManager()
 
 export default class {
   static async acquire (filepath) {
-    await lm.acquire(filepath)
+    // await lm.acquire(filepath)
     if (!map.has(filepath)) {
       // Acquire a file lock while we're reading the index
       // to make sure other processes aren't writing to it
       // simultaneously, which could result in a corrupted index.
-      const fileLock = await Lock(filepath)
+      // const fileLock = await Lock(filepath)
       const rawIndexFile = await read(filepath)
       let index = GitIndex.from(rawIndexFile)
       // cache the GitIndex object so we don't need to re-read it
@@ -23,7 +23,7 @@ export default class {
       // TODO: save the stat data for the index so we know whether
       // the cached file is stale (modified by an outside process).
       map.set(filepath, index)
-      await fileLock.cancel()
+      // await fileLock.cancel()
     }
     return map.get(filepath)
   }
@@ -31,11 +31,11 @@ export default class {
     const index = map.get(filepath)
     if (index && index._dirty) {
       // Acquire a file lock while we're writing the index file
-      let fileLock = await Lock(filepath)
+      // let fileLock = await Lock(filepath)
       const buffer = index.toObject()
-      await fileLock.update(buffer)
+      // await fileLock.update(buffer)
       index._dirty = false
     }
-    lm.release(filepath)
+    // lm.release(filepath)
   }
 }
