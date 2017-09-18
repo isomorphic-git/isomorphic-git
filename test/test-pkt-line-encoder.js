0518502faba1c63489562641c36a989e0f574d95
Will Hilton <wmhilton@gmail.com>
2017-09-17 22:11:07 -0400

    Add utils/pkt-line-encoder

---
+++ test/test-pkt-line-encoder.js
@@ -0,0 +1,20 @@
+import test from 'ava'
+import {encode, flush} from '../lib/utils/pkt-line-encoder.js'
+
+test('pkt-line-encode string', async t => {
+  let foo = encode('hello world\n')
+  t.truthy(foo)
+  t.true(Buffer.compare(foo, Buffer.from('0010hello world\n')) === 0)
+})
+
+test('pkt-line-encode empty', async t => {
+  let foo = encode('')
+  t.truthy(foo)
+  t.true(Buffer.compare(foo, Buffer.from('0004')) === 0)
+})
+
+test('pkt-line flush', async t => {
+  let foo = flush()
+  t.truthy(foo)
+  t.true(Buffer.compare(foo, Buffer.from('0000')) === 0)
+})
