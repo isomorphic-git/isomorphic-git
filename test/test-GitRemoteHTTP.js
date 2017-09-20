064f76e0d60576640c6318de4db6c64646910ae5
Will Hilton <wmhilton@gmail.com>
2017-09-19 23:03:01 -0400

    Start mocking Github so we can test push and pull reliably.

--- test/test-GitRemoteHTTP.js
+++ test/test-GitRemoteHTTP.js
@@ -1,8 +1,27 @@
 import test from 'ava'
 import GitRemoteHTTP from '../lib/managers/GitRemoteHTTP'
+import nock from 'nock'
 
 test('GitRemoteHTTP', async t => {
   let remote = new GitRemoteHTTP('https://github.com/wmhilton/esgit')
+  await remote.discover()
+  console.log(remote)
+  t.truthy(remote)
+})
+
+test('GitRemoteHTTP', async t => {
+  
+  nock('http://example.dev')
+  .get('/test-push.git/info/refs?service=git-upload-pack')
+  // .get(/.*/)
+  .reply(200, `001e# service=git-upload-pack\n00000000`, {
+    'Expires': 'Fri, 01 Jan 1980 00:00:00 GMT',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache, max-age=0, must-revalidate',
+    'Content-Type': 'application/x-git-upload-pack-advertisement'
+  })
+  
+  let remote = new GitRemoteHTTP('http://example.dev/test-push')
   await remote.discover()
   console.log(remote)
   t.truthy(remote)
