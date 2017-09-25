8457e4e1776f38f6ceeb745129b72db232e6389c
Will Hilton <wmhilton@gmail.com>
2017-09-25 14:13:45 -0400

    Fix bug causing early binding to 'fs'.

--- src/add.js
+++ src/add.js
@@ -5,15 +5,13 @@ import GitIndexManager from './managers/GitIndexManager'
 import GitObjectManager from './managers/GitObjectManager'
 import { read } from './managers/models/utils/read'
 
-const lstat = pify(fs.lstat)
-
 export async function add ({ gitdir, workdir, filepath }) {
   const type = 'blob'
   const object = await read(path.join(workdir, filepath))
   if (object === null) throw new Error(`Could not read file '${filepath}'`)
   const oid = await GitObjectManager.write({ gitdir, type, object })
   await GitIndexManager.acquire(`${gitdir}/index`, async function (index) {
-    let stats = await lstat(path.join(workdir, filepath))
+    let stats = await pify(fs.lstat)(path.join(workdir, filepath))
     index.insert({ filepath, stats, oid })
   })
   // TODO: return oid?
--- testling/basic-test.js
+++ testling/basic-test.js
@@ -1,6 +1,8 @@
 const git = require('../dist/for-browserify.js')
 const test = require('tape')
 
+// TODO: get BrowserFS instance
+
 test('things do not explode', t => {
   t.plan(1)
 
