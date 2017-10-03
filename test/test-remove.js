fc6f3394cd8645531afe96caa25570da36db2119
Will Hilton <wmhilton@gmail.com>
2017-10-03 18:39:39 -0400

    Remove more .serial tests

--- test/test-remove.js
+++ test/test-remove.js
@@ -1,13 +1,15 @@
 import test from 'ava'
 import git from '..'
-import { read, write } from '../dist/for-node/utils'
+import pify from 'pify'
+import ncp from 'ncp'
+import { tmpdir } from './_helpers'
 
-test.serial('gitIndex.remove(file)', async t => {
-  await write(
-    'fixtures/test-remove-file.git/index',
-    await read('fixtures/test-remove-file.git/index.orig')
-  )
-  const repo = git().gitdir('fixtures/test-remove-file.git')
+test('remove(file)', async t => {
+  // Setup
+  let clientDir = await tmpdir()
+  await pify(ncp)('fixtures/test-remove.git', clientDir)
+  // Test
+  const repo = git().gitdir(clientDir)
   let before = await repo.list()
   t.snapshot(before)
   await repo.remove('LICENSE.md')
@@ -16,12 +18,12 @@ test.serial('gitIndex.remove(file)', async t => {
   t.true(before.length === after.length + 1)
 })
 
-test.serial('gitIndex.remove(dir)', async t => {
-  await write(
-    'fixtures/test-remove-dir.git/index',
-    await read('fixtures/test-remove-dir.git/index.orig')
-  )
-  const repo = git().gitdir('fixtures/test-remove-dir.git')
+test('remove(dir)', async t => {
+  // Setup
+  let clientDir = await tmpdir()
+  await pify(ncp)('fixtures/test-remove.git', clientDir)
+  // Test
+  const repo = git().gitdir(clientDir)
   let before = await repo.list()
   t.snapshot(before)
   await repo.remove('src/models')
