b985f66343a33c07e22465c7fd7654ae32f1b21f
William Hilton <wmhilton@gmail.com>
2017-10-29 00:29:31 -0400

    Fix premature binding of 'fs'

--- src/utils/readDirAsFlatFileList.js
+++ src/utils/readDirAsFlatFileList.js
@@ -1,17 +1,16 @@
-import { fs } from '../utils'
+import fs from './fs'
 import pify from 'pify'
-
-const readdir = pify(fs().readdir)
-const stat = pify(fs().stat)
 
 // Based on an elegant concurrent recursive solution from SO
 // https://stackoverflow.com/a/45130990/2168416
 export async function readDirAsFlatFileList (dir) {
-  const subdirs = await readdir(dir)
+  const subdirs = await pify(fs().readdir)(dir)
   const files = await Promise.all(
     subdirs.map(async subdir => {
       const res = dir + '/' + subdir
-      return (await stat(res)).isDirectory() ? readDirAsFlatFileList(res) : res
+      return (await pify(fs().stat)(res)).isDirectory()
+        ? readDirAsFlatFileList(res)
+        : res
     })
   )
   return files.reduce((a, f) => a.concat(f), [])
