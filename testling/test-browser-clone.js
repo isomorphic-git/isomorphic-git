78c20d3d10028e9605fa47457055e039ab14ac05
William Hilton <wmhilton@gmail.com>
2017-12-03 23:28:44 -0500

    Revert the Sauce tests back to .then() style

--- testling/test-browser-clone.js
+++ testling/test-browser-clone.js
@@ -4,17 +4,20 @@ const BrowserFS = require('browserfs')
 test('clone', t => {
   t.plan(2)
   BrowserFS.install(window)
-  BrowserFS.configure({ fs: 'LocalStorage' }, async function (err) {
+  BrowserFS.configure({ fs: 'LocalStorage' }, function (err) {
     if (err) return t.fail(err)
     var fs = window.require('fs')
     t.ok(fs, 'Loaded fs')
     repo = new Git({ fs: fs, dir: '.' })
-    await clone(repo, {
+    clone(repo, {
       depth: 1,
       branch: 'master',
       url:
         'https://cors-buster-jfpactjnem.now.sh/github.com/wmhilton/isomorphic-git'
     })
-    t.pass('clone')
+      .then(function () {
+        t.pass('clone')
+      })
+      .catch(t.fail)
   })
 })
