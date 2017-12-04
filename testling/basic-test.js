aa7c31c36b1abb681129e7aa260fadbecf52ba31
William Hilton <wmhilton@gmail.com>
2017-12-03 23:28:44 -0500

    Revert the Sauce tests back to .then() style

--- testling/basic-test.js
+++ testling/basic-test.js
@@ -5,33 +5,42 @@ const BrowserFS = require('browserfs')
 test('things do not explode', t => {
   t.plan(5)
   BrowserFS.install(window)
-  BrowserFS.configure({ fs: 'InMemory' }, async function (err) {
+  BrowserFS.configure({ fs: 'InMemory' }, function (err) {
     if (err) return t.fail(err)
     var fs = window.require('fs')
     t.ok(fs, 'Loaded fs')
 
     let repo = new Git({ fs: fs, dir: '.' })
-    await init(repo)
-    t.pass('init')
+    init(repo)
+      .then(function () {
+        t.pass('init')
 
-    fs.writeFileSync('a.txt', 'Hello', 'utf8')
-    await add(repo, { filepath: 'a.txt' })
-    t.pass('add a.txt')
+        fs.writeFileSync('a.txt', 'Hello', 'utf8')
+        add(repo, { filepath: 'a.txt' })
+          .then(function () {
+            t.pass('add a.txt')
 
-    let oid = await commit(repo, {
-      author: {
-        name: 'Mr. Test',
-        email: 'mrtest@example.com',
-        timestamp: 1262356920
-      },
-      message: 'Initial commit'
-    })
-    t.pass('commit')
+            commit(repo, {
+              author: {
+                name: 'Mr. Test',
+                email: 'mrtest@example.com',
+                timestamp: 1262356920
+              },
+              message: 'Initial commit'
+            })
+              .then(function (oid) {
+                t.pass('commit')
 
-    t.equal(
-      oid,
-      'fbe80a5f33d7876603767211bd6d53d3e308894e',
-      "- oid is 'fbe80a5f33d7876603767211bd6d53d3e308894e'"
-    )
+                t.equal(
+                  oid,
+                  'fbe80a5f33d7876603767211bd6d53d3e308894e',
+                  "- oid is 'fbe80a5f33d7876603767211bd6d53d3e308894e'"
+                )
+              })
+              .catch(t.fail)
+          })
+          .catch(t.fail)
+      })
+      .catch(t.fail)
   })
 })
