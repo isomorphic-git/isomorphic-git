abaedc78a6f6fff4e3f2cd7a5ac5107bc95cf343
William Hilton <wmhilton@gmail.com>
2017-12-03 23:10:23 -0500

    Update browser tests to use async/await

--- testling/test-browser-clone.js
+++ testling/test-browser-clone.js
@@ -4,20 +4,17 @@ const BrowserFS = require('browserfs')
 test('clone', t => {
   t.plan(2)
   BrowserFS.install(window)
-  BrowserFS.configure({ fs: 'LocalStorage' }, function (err) {
+  BrowserFS.configure({ fs: 'LocalStorage' }, async function (err) {
     if (err) return t.fail(err)
     var fs = window.require('fs')
     t.ok(fs, 'Loaded fs')
     repo = new Git({ fs: fs, dir: '.' })
-    clone(repo, {
+    await clone(repo, {
       depth: 1,
       branch: 'master',
       url:
         'https://cors-buster-jfpactjnem.now.sh/github.com/wmhilton/isomorphic-git'
     })
-      .then(function () {
-        t.pass('clone')
-      })
-      .catch(t.fail)
+    t.pass('clone')
   })
 })
