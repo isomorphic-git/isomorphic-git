2b819c337801efaf6f7d1f81942a291b754fcb41
William Hilton <wmhilton@gmail.com>
2017-12-19 23:57:51 -0500

    Start implementing GitPackfile.read

--- __tests__/test-GitPackfile.js
+++ __tests__/test-GitPackfile.js
@@ -1,20 +1,27 @@
 /* globals describe test expect */
-import { FileSystem, GitPackfile } from '../dist/for-node/models'
+import { FileSystem, GitPackfile, GitObject } from '../dist/for-node/models'
 import _fs from 'fs'
 const fs = new FileSystem(_fs)
 
+const loadPackfile = async () =>
+  GitPackfile.fromIDX({
+    idx: await fs.read(
+      '__tests__/__fixtures__/test-packfile.git/objects/pack/pack-1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888.idx'
+    ),
+    pack: await fs.read(
+      '__tests__/__fixtures__/test-packfile.git/objects/pack/pack-1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888.pack'
+    )
+  })
+
 describe('GitPackfile', () => {
   test('size', async () => {
-    let p = await GitPackfile.fromIDX({
-      idx: await fs.read(
-        '__tests__/__fixtures__/test-packfile.git/objects/pack/pack-1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888.idx'
-      ),
-      pack: await fs.read(
-        '__tests__/__fixtures__/test-packfile.git/objects/pack/pack-1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888.pack'
-      )
-    })
+    let p = await loadPackfile()
     expect(p.size).toBe(769)
     expect(p.hashes).toMatchSnapshot()
+    expect(p.packfileSha).toBe('1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888')
+  })
+  test('offsets', async () => {
+    let p = await loadPackfile()
     // Test a handful of known offsets + lengths.
     expect(p.slices.get('0b8faa11b353db846b40eb064dfb299816542a46')).toEqual([
       40077,
@@ -32,10 +39,32 @@ describe('GitPackfile', () => {
       38202,
       38202 + 834
     ])
-    expect(p.packfileSha).toBe('1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888')
-    // console.log(p.pack.slice()
-    // let blob = await p.read({oid: '637c4e69d85e0dcc18898ec251377453d0891585'})
-    // let {type, object} = GitObject.unwrap({oid: '637c4e69d85e0dcc18898ec251377453d0891585', file: blob})
-    // console.log(type, object)
+    expect(p.slices.get('5f1f014326b1d7e8079d00b87fa7a9913bd91324')).toEqual([
+      20855,
+      20855 + 31
+    ])
+  })
+  test('read undeltified object', async () => {
+    let p = await loadPackfile()
+    let { type, object } = await p.read({
+      oid: '637c4e69d85e0dcc18898ec251377453d0891585'
+    })
+    // console.log(object.toString('utf8'))
+    expect(type).toBe('commit')
+    expect(object.toString('utf8')).toMatchSnapshot()
+    let { oid } = GitObject.wrap({ type, object })
+    expect(oid).toBe('637c4e69d85e0dcc18898ec251377453d0891585')
+  })
+  test('read deltified object', async () => {
+    '5f1f014326b1d7e8079d00b87fa7a9913bd91324' // 'c2ed6204dc5e2293031b11f77f5f3d934a1f3a05'
+    let p = await loadPackfile()
+    let { type, object } = await p.read({
+      oid: '5f1f014326b1d7e8079d00b87fa7a9913bd91324'
+    })
+    expect(type).toBe('commit')
+    console.log(object.toString('utf8'))
+    expect(object.toString('utf8')).toMatchSnapshot()
+    let { oid } = GitObject.wrap({ type, object })
+    expect(oid).toBe('5f1f014326b1d7e8079d00b87fa7a9913bd91324')
   })
 })
