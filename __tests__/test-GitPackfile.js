86f2922320afd558c86f44326d5ed8d6df97c8e1
William Hilton <wmhilton@gmail.com>
2017-12-20 23:40:19 -0500

    Working GitPackfile.read() and GitPackfile.writeIDX(), even OFS deltas finally

--- __tests__/test-GitPackfile.js
+++ __tests__/test-GitPackfile.js
@@ -49,7 +49,6 @@ describe('GitPackfile', () => {
     let { type, object } = await p.read({
       oid: '637c4e69d85e0dcc18898ec251377453d0891585'
     })
-    // console.log(object.toString('utf8'))
     expect(type).toBe('commit')
     expect(object.toString('utf8')).toMatchSnapshot()
     let { oid } = GitObject.wrap({ type, object })
@@ -59,12 +58,26 @@ describe('GitPackfile', () => {
     '5f1f014326b1d7e8079d00b87fa7a9913bd91324' // 'c2ed6204dc5e2293031b11f77f5f3d934a1f3a05'
     let p = await loadPackfile()
     let { type, object } = await p.read({
-      oid: '5f1f014326b1d7e8079d00b87fa7a9913bd91324'
+      oid: '7fb539a8e8488c3fd2793e7dda8a44693e25cce1' // 9 levels deep of deltification.
     })
-    expect(type).toBe('commit')
-    console.log(object.toString('utf8'))
+    expect(type).toBe('blob')
     expect(object.toString('utf8')).toMatchSnapshot()
     let { oid } = GitObject.wrap({ type, object })
-    expect(oid).toBe('5f1f014326b1d7e8079d00b87fa7a9913bd91324')
+    expect(oid).toBe('7fb539a8e8488c3fd2793e7dda8a44693e25cce1')
+  })
+  test('write IDX', async () => {
+    let originalIDX = await fs.read(
+      '__tests__/__fixtures__/test-packfile.git/objects/pack/pack-1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888.idx'
+    )
+    let packfile = await GitPackfile.fromIDX({
+      idx: originalIDX,
+      pack: await fs.read(
+        '__tests__/__fixtures__/test-packfile.git/objects/pack/pack-1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888.pack'
+      )
+    })
+    let idx = await packfile.writeIDX()
+    await fs.write('IDXFILE.idx', idx)
+    expect(idx.byteLength).toBe(originalIDX.byteLength)
+    expect(idx.equals(originalIDX)).toBe(true)
   })
 })
