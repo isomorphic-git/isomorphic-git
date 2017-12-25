4897bf36275c039a39bbb158fe3d1455516b052e
William Hilton <wmhilton@gmail.com>
2017-12-25 02:39:06 -0500

    Cleanup and simplification, change Maps to Objects for (possible?) perf gains

--- __tests__/test-GitPackIndex.js
+++ __tests__/test-GitPackIndex.js
@@ -15,11 +15,11 @@ describe('GitPackIndex', () => {
     expect(p.hashes).toMatchSnapshot()
     expect(p.packfileSha).toBe('1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888')
     // Test a handful of known offsets + lengths.
-    expect(p.offsets.get('0b8faa11b353db846b40eb064dfb299816542a46')).toEqual(40077)
-    expect(p.offsets.get('637c4e69d85e0dcc18898ec251377453d0891585')).toEqual(39860)
-    expect(p.offsets.get('98e9fde3ee878fa985a143fc5fe05d4e6d8e637b')).toEqual(39036)
-    expect(p.offsets.get('43c49edb213748626fc363c890c01a9e55a1b8da')).toEqual(38202)
-    expect(p.offsets.get('5f1f014326b1d7e8079d00b87fa7a9913bd91324')).toEqual(20855)
+    expect(p.offsets['0b8faa11b353db846b40eb064dfb299816542a46']).toEqual(40077)
+    expect(p.offsets['637c4e69d85e0dcc18898ec251377453d0891585']).toEqual(39860)
+    expect(p.offsets['98e9fde3ee878fa985a143fc5fe05d4e6d8e637b']).toEqual(39036)
+    expect(p.offsets['43c49edb213748626fc363c890c01a9e55a1b8da']).toEqual(38202)
+    expect(p.offsets['5f1f014326b1d7e8079d00b87fa7a9913bd91324']).toEqual(20855)
     // expect(p.slices.get('0b8faa11b353db846b40eb064dfb299816542a46')).toEqual([
     //   40077,
     //   40077 + 192
@@ -50,11 +50,11 @@ describe('GitPackIndex', () => {
     expect(p.hashes).toMatchSnapshot()
     expect(p.packfileSha).toBe('1a1e70d2f116e8cb0cb42d26019e5c7d0eb01888')
     // Test a handful of known offsets + lengths.
-    expect(p.offsets.get('0b8faa11b353db846b40eb064dfb299816542a46')).toEqual(40077)
-    expect(p.offsets.get('637c4e69d85e0dcc18898ec251377453d0891585')).toEqual(39860)
-    expect(p.offsets.get('98e9fde3ee878fa985a143fc5fe05d4e6d8e637b')).toEqual(39036)
-    expect(p.offsets.get('43c49edb213748626fc363c890c01a9e55a1b8da')).toEqual(38202)
-    expect(p.offsets.get('5f1f014326b1d7e8079d00b87fa7a9913bd91324')).toEqual(20855)
+    expect(p.offsets['0b8faa11b353db846b40eb064dfb299816542a46']).toEqual(40077)
+    expect(p.offsets['637c4e69d85e0dcc18898ec251377453d0891585']).toEqual(39860)
+    expect(p.offsets['98e9fde3ee878fa985a143fc5fe05d4e6d8e637b']).toEqual(39036)
+    expect(p.offsets['43c49edb213748626fc363c890c01a9e55a1b8da']).toEqual(38202)
+    expect(p.offsets['5f1f014326b1d7e8079d00b87fa7a9913bd91324']).toEqual(20855)
     // expect(p.slices.get('0b8faa11b353db846b40eb064dfb299816542a46')).toEqual([
     //   40077,
     //   40077 + 192
