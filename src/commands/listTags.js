229febb48aa7e3da4b0cf0123b65637fdba5352a
William Hilton <wmhilton@gmail.com>
2018-01-14 19:08:18 -0500

    feat: Add 'remote' argument to listBranches and add new 'listTags' command

--- src/commands/listTags.js
+++ src/commands/listTags.js
@@ -3,26 +3,24 @@ import { GitRefManager } from '../managers'
 import { FileSystem } from '../models'
 
 /**
- * List branches
+ * List all local tags
  *
  * @param {Object} args - Arguments object
  * @param {FSModule} args.fs - The filesystem holding the git repo
  * @param {string} args.dir - The path to the [working tree](index.html#dir-vs-gitdir) directory
  * @param {string} [args.gitdir=path.join(dir, '.git')] - The path to the [git directory](index.html#dir-vs-gitdir)
- * @param {string} [remote=undefined] - If specified, lists the branches for that remote. Otherwise lists local branches.
  * @returns {Promise<string[]>} - Resolves successfully with an array of branch names.
  *
  * @example
  * let repo = {fs, dir: '<@.@>'}
- * let branches = await git.listBranches(repo)
- * console.log(branches)
+ * let tags = await git.listTags(repo)
+ * console.log(tags)
  */
-export async function listBranches ({
+export async function listTags ({
   dir,
   gitdir = path.join(dir, '.git'),
-  fs: _fs,
-  remote = undefined
+  fs: _fs
 }) {
   const fs = new FileSystem(_fs)
-  return GitRefManager.listBranches({ fs, gitdir, remote })
+  return GitRefManager.listTags({ fs, gitdir })
 }
