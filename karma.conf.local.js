95b3de82f3d4336617a69ba86891cb213544c828
Will Hilton <wmhilton@gmail.com>
2018-02-01 19:44:02 -0500

    Modify SauceLabs config to work with "untrusted pull requests".

--- karma.conf.local.js
+++ karma.conf.local.js
@@ -10,9 +10,6 @@ module.exports = Object.assign({}, base, {
   port: 9876,
   // enable / disable watching file and executing tests whenever any file changes
   autoWatch: true,
-  // start these browsers
-  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-  browsers: ['Firefox'],
   // Continuous Integration mode
   // if true, Karma captures browsers, runs the tests and exits
   singleRun: false,
