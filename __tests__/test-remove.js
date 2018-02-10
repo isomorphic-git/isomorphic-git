910f21019f39e732adf57bc130329720b60cc2ba
William Hilton <wmhilton@gmail.com>
2018-02-09 22:22:05 -0500

    More makeFixture consolidation (#69)
    * Upgraded the following tests to use makeFixture (status, resolveRef, checkout, readObject, remove)

--- __tests__/test-remove.js
+++ __tests__/test-remove.js
@@ -1,32 +1,29 @@
 /* global test describe expect */
-import fs from 'fs'
-import { copyFixtureIntoTempDir } from 'jest-fixtures'
-import { remove, listFiles } from 'isomorphic-git'
+const { makeFixture } = require('./__helpers__/FixtureFS.js')
 
-/** @test {remove} */
+const { remove, listFiles } = require('isomorphic-git')
+
+
 describe('remove', () => {
-  test('file', async () => {
+  it('file', async () => {
     // Setup
-    let gitdir = await copyFixtureIntoTempDir(__dirname, 'test-remove.git')
+    let { fs, gitdir } = await makeFixture('test-remove')
     // Test
-    const repo = { fs, gitdir }
-    let before = await listFiles(repo)
+    let before = await listFiles({ fs, gitdir })
     expect(before).toMatchSnapshot()
-    await remove({ ...repo, filepath: 'LICENSE.md' })
-    let after = await listFiles(repo)
+    await remove({ fs, gitdir, filepath: 'LICENSE.md' })
+    let after = await listFiles({ fs, gitdir })
     expect(after).toMatchSnapshot()
     expect(before.length === after.length + 1).toBe(true)
   })
-
-  test('dir', async () => {
+  it('dir', async () => {
     // Setup
-    let gitdir = await copyFixtureIntoTempDir(__dirname, 'test-remove.git')
+    let { fs, gitdir } = await makeFixture('test-remove')
     // Test
-    const repo = { fs, gitdir }
-    let before = await listFiles(repo)
+    let before = await listFiles({ fs, gitdir })
     expect(before).toMatchSnapshot()
-    await remove({ ...repo, filepath: 'src/models' })
-    let after = await listFiles(repo)
+    await remove({ fs, gitdir, filepath: 'src/models' })
+    let after = await listFiles({ fs, gitdir })
     expect(after).toMatchSnapshot()
     expect(before.length === after.length + 5).toBe(true)
   })
