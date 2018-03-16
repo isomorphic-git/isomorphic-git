5f1530c99086baf6e248007604b2b11d61d71d25
William Hilton <wmhilton@gmail.com>
2018-03-16 08:31:23 -0400

    fix: Cloning with a tag for 'ref' now works (fixes #93) (PR #99)

--- __tests__/test-clone.js
+++ __tests__/test-clone.js
@@ -25,4 +25,27 @@ describe('clone', () => {
     expect(fs.existsSync(`${gitdir}/refs/heads/test-branch`)).toBe(true)
     expect(fs.existsSync(`${dir}/package.json`)).toBe(true)
   })
+  it('clone a tag', async () => {
+    let { fs, dir, gitdir } = await makeFixture('isomorphic-git')
+    let url = `https://${
+      process.browser ? 'cors-buster-jfpactjnem.now.sh/' : ''
+    }github.com/isomorphic-git/isomorphic-git`
+    await clone({
+      fs,
+      dir,
+      gitdir,
+      depth: 1,
+      singleBranch: true,
+      ref: 'test-tag',
+      url
+    })
+    expect(fs.existsSync(`${dir}`)).toBe(true)
+    expect(fs.existsSync(`${gitdir}/objects`)).toBe(true)
+    expect(fs.existsSync(`${gitdir}/refs/remotes/origin/test-tag`)).toBe(
+      false
+    )
+    expect(fs.existsSync(`${gitdir}/refs/heads/test-tag`)).toBe(false)
+    expect(fs.existsSync(`${gitdir}/refs/tags/test-tag`)).toBe(true)
+    expect(fs.existsSync(`${dir}/package.json`)).toBe(true)
+  })
 })
