d66ec5299c2380df023b47a98df4534a44a6bfb2
William Hilton <wmhilton@gmail.com>
2018-04-07 22:19:45 -0400

    fix: current version number (#130)

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,18 +1,5 @@
 import { pkg } from '../utils/pkg'
 
-/**
- * Return the version number of 'isomorphic-git'
- *
- * I don't know why you might need this. I added it just so I could check that I was getting
- * the correct version of the library and not a cached version.
- *
- * TODO: Semantic-release broke this, now it always says '0.0.0-development'. Need to add a
- * prepublishOnly script to find & replace that with the actual version number.
- *
- * @returns {string} version - the version string taken from package.json at publication time
- * @example
- * console.log(git.version())
- */
 export function version () {
   return pkg.version
 }
