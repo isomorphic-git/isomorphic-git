24aced9be045b63691fe57c93275cda861847f66
Will Hilton <wmhilton@gmail.com>
2018-04-14 18:09:54 -0400

    chore(format): Organize exports

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
+export { auth } from './utils/auth'
 export {
   flatFileListToDirectoryStructure
 } from './utils/flatFileListToDirectoryStructure'
-export { sleep } from './utils/sleep'
-export { pkg } from './utils/pkg'
-export { oauth2 } from './utils/oauth2'
-export { auth } from './utils/auth'
 export { log } from './utils/log'
+export { oauth2 } from './utils/oauth2'
+export { pkg } from './utils/pkg'
+export { sleep } from './utils/sleep'
