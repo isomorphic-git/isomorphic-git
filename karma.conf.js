3cc5f1a797fb40dacbd3f4b496174587279e4cb9
Will Hilton <wmhilton@gmail.com>
2018-05-12 15:07:15 -0400

    Try using ChromeHeadless on Travis again

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,5 @@
 // Karma configuration
+process.env.CHROME_BIN = require('puppeteer').executablePath()
 const path = require('path')
 const webpack = require('webpack')
 
@@ -10,7 +11,7 @@ module.exports = function (config) {
   const options = {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['FirefoxHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless'],
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
     // frameworks to use
@@ -93,6 +94,10 @@ module.exports = function (config) {
       FirefoxHeadless: {
         base: 'Firefox',
         flags: ['-headless']
+      },
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
       }
     },
     sauceLabs: {
@@ -164,7 +169,6 @@ module.exports = function (config) {
   }
 
   if (!process.env.CI) {
-    options.browsers.push('ChromeHeadless')
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     options.singleRun = false
