0f233b1d0be92420737d0d9d620b997ddb84b1a8
William Hilton <wmhilton@gmail.com>
2018-05-27 01:05:57 -0400

    feat: Added 'currentBranch' command (#241)

---
+++ src/commands/currentBranch.js
@@ -0,0 +1,45 @@
+import path from 'path'
+
+import { GitRefManager } from '../managers'
+import { FileSystem } from '../models'
+
+// @see https://git-scm.com/docs/git-rev-parse.html#_specifying_revisions
+const regexs = [
+  new RegExp('refs/remotes/(.*)/HEAD'),
+  new RegExp('refs/remotes/(.*)'),
+  new RegExp('refs/heads/(.*)'),
+  new RegExp('refs/tags/(.*)'),
+  new RegExp('refs/(.*)')
+]
+
+function abbreviate (ref) {
+  for (const reg of regexs) {
+    let matches = reg.exec(ref)
+    if (matches) {
+      return matches[1]
+    }
+  }
+  return ref
+}
+
+/**
+ * Get the name of the branch currently pointed to by .git/HEAD
+ *
+ * @link https://isomorphic-git.github.io/docs/currentBranch.html
+ */
+export async function currentBranch ({
+  dir,
+  gitdir = path.join(dir, '.git'),
+  fs: _fs,
+  fullname = false
+}) {
+  const fs = new FileSystem(_fs)
+  let ref = await GitRefManager.resolve({
+    fs,
+    gitdir,
+    ref: 'HEAD',
+    depth: 2
+  })
+  if (fullname) return ref
+  return abbreviate(ref)
+}
