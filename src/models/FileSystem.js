f6c107ce41be23a391d9575eea7409512e1c25ed
William Hilton <wmhilton@gmail.com>
2018-06-24 15:32:25 -0400

    perf: slightly faster packfile idx loading times (#295)

--- src/models/FileSystem.js
+++ src/models/FileSystem.js
@@ -1,8 +1,12 @@
+import debug from 'debug'
 import path from 'path'
 import pify from 'pify'
+import * as marky from 'marky'
 
 import { E, GitError } from '../models/GitError'
 import { sleep } from '../utils'
+
+const readFileLog = debug('readFile')
 
 const delayedReleases = new Map()
 /**
@@ -42,7 +46,9 @@ export class FileSystem {
    */
   async read (filepath, options = {}) {
     try {
+      marky.mark(filepath)
       let buffer = await this._readFile(filepath, options)
+      readFileLog(`${filepath} ${marky.stop(filepath).duration}`)
       return buffer
     } catch (err) {
       return null
