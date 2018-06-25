c4dfc486ca56e7bd7c0c19bedc1bd663a65cfb80
akaJes <akaJes@users.noreply.github.com>
2018-06-25 11:10:24 +0300

    Update GitPackIndex.js

--- src/models/GitPackIndex.js
+++ src/models/GitPackIndex.js
@@ -54,7 +54,6 @@ function otherVarIntDecode (reader, startWith) {
 export class GitPackIndex {
   constructor (stuff) {
     Object.assign(this, stuff)
-    this.hashesCache = (this.hashes || []).reduce((p, i) => ((p[i] = i), p), {})
     this.offsetCache = {}
   }
   static async fromIdx ({ idx, getExternalRefDelta }) {
