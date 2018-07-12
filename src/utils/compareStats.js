32a78b78e572892629e398fd1f7ce74b317a32a0
William Hilton <wmhilton@gmail.com>
2018-07-12 14:45:06 -0400

    fix: cache stat values after calling git.status (#326)
    * perf: speed up git.status
    
    * handle edge case stats.size === -1

---
+++ src/utils/compareStats.js
@@ -0,0 +1,19 @@
+import { normalizeStats } from './normalizeStats'
+
+export function compareStats ({ entry, stats }) {
+  // Comparison based on the description in Paragraph 4 of
+  // https://www.kernel.org/pub/software/scm/git/docs/technical/racy-git.txt
+  const e = normalizeStats(entry)
+  const s = normalizeStats(stats)
+  const staleness =
+    e.mode !== s.mode ||
+    e.mtimeSeconds !== s.mtimeSeconds ||
+    e.ctimeSeconds !== s.ctimeSeconds ||
+    e.uid !== s.uid ||
+    e.gid !== s.gid ||
+    e.ino !== s.ino ||
+    e.size !== s.size
+  // console.log(staleness ? 'stale:' : 'fresh:')
+  // console.table([e, s])
+  return staleness
+}
