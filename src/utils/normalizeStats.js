9bfe92790c3d07ed055c01b41ef3e86f97ea01fb
William Hilton <wmhilton@gmail.com>
2018-07-21 18:43:47 -0400

    feat: Added 'isDescendent' command

--- src/utils/normalizeStats.js
+++ src/utils/normalizeStats.js
@@ -1,18 +1,22 @@
 const MAX_UINT32 = 2 ** 32
 
 export function normalizeStats (e) {
-  const ctimeSeconds = e.ctimeSeconds !== undefined
-    ? e.ctimeSeconds
-    : Math.floor(e.ctime.valueOf() / 1000)
-  const mtimeSeconds = e.mtimeSeconds !== undefined
-    ? e.mtimeSeconds
-    : Math.floor(e.mtime.valueOf() / 1000)
-  const ctimeNanoseconds = e.ctimeNanoseconds !== undefined
-    ? e.ctimeNanoseconds
-    : (e.ctime.valueOf() - ctimeSeconds * 1000) * 1000000
-  const mtimeNanoseconds = e.mtimeNanoseconds !== undefined
-    ? e.mtimeNanoseconds
-    : (e.mtime.valueOf() - mtimeSeconds * 1000) * 1000000
+  const ctimeSeconds =
+    e.ctimeSeconds !== undefined
+      ? e.ctimeSeconds
+      : Math.floor(e.ctime.valueOf() / 1000)
+  const mtimeSeconds =
+    e.mtimeSeconds !== undefined
+      ? e.mtimeSeconds
+      : Math.floor(e.mtime.valueOf() / 1000)
+  const ctimeNanoseconds =
+    e.ctimeNanoseconds !== undefined
+      ? e.ctimeNanoseconds
+      : (e.ctime.valueOf() - ctimeSeconds * 1000) * 1000000
+  const mtimeNanoseconds =
+    e.mtimeNanoseconds !== undefined
+      ? e.mtimeNanoseconds
+      : (e.mtime.valueOf() - mtimeSeconds * 1000) * 1000000
 
   return {
     ctimeSeconds: ctimeSeconds % MAX_UINT32,
