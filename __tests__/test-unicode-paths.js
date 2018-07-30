d84d9f5d070f7286f5961a8f1cb383269444faa4
Koutaro Chikuba <miz404@gmail.com>
2018-07-31 00:31:47 +0900

    fix: another fix for multibyte utf8 characters (#357)
    * fix: multibyte under dir (#354)
    * Add @mizchi as a contributor

--- __tests__/test-unicode-paths.js
+++ __tests__/test-unicode-paths.js
@@ -1,4 +1,6 @@
 /* eslint-env node, browser, jasmine */
+const path = require('path')
+const pify = require('pify')
 const { makeFixture } = require('./__helpers__/FixtureFS.js')
 
 const {
@@ -20,6 +22,18 @@ describe('unicode filepath support', () => {
     await add({ fs, dir, gitdir, filepath: '日本語' })
     expect((await listFiles({ fs, dir, gitdir }))[0]).toBe('日本語')
     await remove({ fs, dir, gitdir, filepath: '日本語' })
+    expect((await listFiles({ fs, dir, gitdir })).length).toBe(0)
+  })
+  it('write/read index docs/日本語', async () => {
+    // Setup
+    let { fs, dir, gitdir } = await makeFixture('test-unicode-paths')
+    await init({ fs, dir, gitdir })
+    // Test
+    await pify(fs.mkdir)(path.join(dir, 'docs'))
+    await pify(fs.writeFile)(path.join(dir, 'docs/日本語'), '')
+    await add({ fs, dir, gitdir, filepath: 'docs/日本語' })
+    expect((await listFiles({ fs, dir, gitdir }))[0]).toBe('docs/日本語')
+    await remove({ fs, dir, gitdir, filepath: 'docs/日本語' })
     expect((await listFiles({ fs, dir, gitdir })).length).toBe(0)
   })
   it('write/read commit 日本語', async () => {
@@ -91,5 +105,30 @@ describe('unicode filepath support', () => {
     // Check GitIndex object
     await checkout({ fs, dir, gitdir, ref: 'HEAD' })
     expect((await listFiles({ fs, dir, gitdir }))[0]).toBe('日本語')
+  })
+  it('checkout docs/日本語', async () => {
+    // Setup
+    let { fs, dir, gitdir } = await makeFixture('test-unicode-paths')
+    await pify(fs.mkdir)(path.join(dir, 'docs'))
+    await pify(fs.writeFile)(path.join(dir, 'docs/日本語'), '')
+    await init({ fs, dir, gitdir })
+    await add({ fs, dir, gitdir, filepath: 'docs/日本語' })
+    await commit({
+      fs,
+      dir,
+      gitdir,
+      author: {
+        name: '日本語',
+        email: '日本語@example.com',
+        timestamp: 1262356920,
+        timezoneOffset: -0
+      },
+      message: '日本語'
+    })
+    await remove({ fs, dir, gitdir, filepath: 'docs/日本語' })
+    // Test
+    // Check GitIndex object
+    await checkout({ fs, dir, gitdir, ref: 'HEAD' })
+    expect((await listFiles({ fs, dir, gitdir }))[0]).toBe('docs/日本語')
   })
 })
