1dfed71748d79513b193d8692bfe6ce40d94d0d3
William Hilton <wmhilton@gmail.com>
2018-08-22 21:37:49 -0400

    clean up

--- src/commands/resetIndex.js
+++ src/commands/resetIndex.js
@@ -8,7 +8,7 @@ import { FileSystem } from '../models/FileSystem.js'
 import { readObject } from './readObject'
 
 /**
- * Reset a file from the git index (aka staging area)
+ * Reset a file in the git index (aka staging area)
  *
  * @link https://isomorphic-git.github.io/docs/resetIndex.html
  */
@@ -21,22 +21,40 @@ export async function resetIndex ({
 }) {
   try {
     const fs = new FileSystem(_fs)
+    // Resolve commit
     let oid = await GitRefManager.resolve({ fs, gitdir, ref })
     let workdirOid
-    if (oid) {
-      try {
-        const obj = await readObject({ gitdir, fs, oid, filepath, format: 'deflated' })
-        oid = obj && obj.oid
-      } catch (e) {
-        oid = null
-      }
-      const object = await fs.read(path.join(dir, filepath))
-      if (object) {
-        workdirOid = await GitObjectManager.hash({
-          gitdir,
-          type: 'blob',
-          object
-        })
+    try {
+      // Resolve blob
+      const obj = await readObject({ gitdir, fs, oid, filepath, format: 'deflated' })
+      oid = obj && obj.oid
+    } catch (e) {
+      // This means we're resetting the file to a "deleted" state
+      oid = null
+    }
+    // For files that aren't in the workdir use zeros
+    let stats = {
+      ctime: new Date(0),
+      mtime: new Date(0),
+      dev: 0,
+      ino: 0,
+      mode: 0,
+      uid: 0,
+      gid: 0,
+      size: 0
+    }
+    // If the file exists in the workdir...
+    const object = await fs.read(path.join(dir, filepath))
+    if (object) {
+      // ... and has the same hash as the desired state...
+      workdirOid = await GitObjectManager.hash({
+        gitdir,
+        type: 'blob',
+        object
+      })
+      if (oid === workdirOid) {
+        // ... use the workdir Stats object
+        stats = await fs.lstat(path.join(dir, filepath))
       }
     }
     await GitIndexManager.acquire(
@@ -44,16 +62,6 @@ export async function resetIndex ({
       async function (index) {
         index.delete({ filepath })
         if (oid) {
-          let stats = { ctime: new Date(0), mtime: new Date(0), dev: 0, ino: 0, mode: 0, uid: 0, gid: 0, size: 0 }
-          if (oid === workdirOid) {
-            try {
-              stats = await fs._lstat(path.join(dir, filepath))
-            } catch (err) {
-              if (err.code !== 'ENOENT') {
-                throw err
-              }
-            }
-          }
           index.insert({ filepath, stats, oid })
         }
       }
