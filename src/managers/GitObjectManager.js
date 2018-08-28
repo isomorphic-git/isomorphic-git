0f28bfa1693f609970140d16ea084593c27ee4dd
William Hilton <wmhilton@gmail.com>
2018-08-28 19:20:09 -0400

    split GitObjectManager in two

--- src/managers/GitObjectManager.js
+++ src/managers/GitObjectManager.js
@@ -1,128 +1,74 @@
 import pako from 'pako'
-import path from 'path'
 
+import * as GitObjectStoreLoose from '../managers/GitObjectStoreLoose.js'
+import * as GitObjectStorePacked from '../managers/GitObjectStorePacked.js'
 import { FileSystem } from '../models/FileSystem.js'
 import { E, GitError } from '../models/GitError.js'
 import { GitObject } from '../models/GitObject.js'
-import { GitPackIndex } from '../models/GitPackIndex.js'
 import { shasum } from '../utils/shasum.js'
-
-const PackfileCache = new Map()
 
 export class GitObjectManager {
   static async read ({ fs: _fs, gitdir, oid, format = 'content' }) {
     const fs = new FileSystem(_fs)
+    // Curry the current read method so that the packfile un-deltification
+    // process can acquire external ref-deltas.
+    const getExternalRefDelta = oid =>
+      GitObjectManager.read({ fs: _fs, gitdir, oid })
     // Look for it in the loose object directory.
-    let file = await fs.read(
-      `${gitdir}/objects/${oid.slice(0, 2)}/${oid.slice(2)}`
-    )
-    let source = `./objects/${oid.slice(0, 2)}/${oid.slice(2)}`
+    let result = await GitObjectStoreLoose.read({ fs, gitdir, oid })
     // Check to see if it's in a packfile.
-    if (!file) {
-      // Curry the current read method so that the packfile un-deltification
-      // process can acquire external ref-deltas.
-      const getExternalRefDelta = oid =>
-        GitObjectManager.read({ fs: _fs, gitdir, oid })
-      // Iterate through all the .pack files
-      let list = await fs.readdir(path.join(gitdir, '/objects/pack'))
-      list = list.filter(x => x.endsWith('.pack'))
-      for (let filename of list) {
-        // Try to get the packfile from the in-memory cache
-        let p = PackfileCache.get(filename)
-        const packFile = `${gitdir}/objects/pack/${filename}`
-        if (!p) {
-          p = GitObjectManager.loadPack(fs, packFile, getExternalRefDelta)
-          PackfileCache.set(filename, p)
-        }
-        // console.log(p)
-        // If the packfile DOES have the oid we're looking for...
-        p = await p
-        if (p.error) throw new GitError(E.InternalFail, { message: p.error })
-        if (p.offsets.has(oid)) {
-          // Get the resolved git object from the packfile
-          if (!p.pack) {
-            p.pack = fs.read(packFile)
-          }
-          let result = await p.read({ oid, getExternalRefDelta })
-          result.source = `./objects/pack/${filename}`
-          return result
-        }
-      }
+    if (!result) {
+      result = await GitObjectStorePacked.read({ fs, gitdir, oid, getExternalRefDelta })
     }
     // Finally
-    if (!file) {
+    if (!result) {
       throw new GitError(E.ReadObjectFail, { oid })
     }
     if (format === 'deflated') {
-      return { format: 'deflated', object: file, source }
+      return result
     }
-    let buffer = Buffer.from(pako.inflate(file))
-    if (format === 'wrapped') {
-      return { format: 'wrapped', object: buffer, source }
+    // BEHOLD! THE ONLY TIME I'VE EVER WANTED TO USE A CASE STATEMENT WITH FOLLOWTHROUGH!
+    // eslint-ignore
+    /* eslint-disable no-fallthrough */
+    switch (result.format) {
+      case 'deflated':
+        let buffer = Buffer.from(pako.inflate(result.object))
+        result = { format: 'wrapped', object: buffer, source: result.source }
+      case 'wrapped':
+        if (format === 'wrapped' && result.format === 'wrapped') {
+          return result
+        }
+        let sha = shasum(result.object)
+        if (sha !== oid) {
+          throw new GitError(E.InternalFail, {
+            message: `SHA check failed! Expected ${oid}, computed ${sha}`
+          })
+        }
+        let { object, type } = GitObject.unwrap(buffer)
+        result = { type, format: 'content', object, source: result.source }
+      case 'content':
+        if (format === 'content') return result
+        break
+      default:
+        throw new GitError(E.InternalFail, {
+          message: `invalid format "${result.format}"`
+        })
     }
-    let sha = shasum(buffer)
-    if (sha !== oid) {
-      throw new GitError(E.InternalFail, {
-        message: `SHA check failed! Expected ${oid}, computed ${sha}`
-      })
-    }
-    let { object, type } = GitObject.unwrap(buffer)
-    if (format === 'content') return { type, format: 'content', object, source }
-  }
-  static async loadPack (fs, filename, getExternalRefDelta) {
-    // If not there, load it from a .idx file
-    const idxName = filename.replace(/pack$/, 'idx')
-    if (await fs.exists(idxName)) {
-      const idx = await fs.read(idxName)
-      return GitPackIndex.fromIdx({ idx, getExternalRefDelta })
-    }
-    // If the .idx file isn't available, generate one.
-    const pack = await fs.read(filename)
-    // Sanity check. 12 byte header + 20 byte shasum
-    if (pack.length < 32) {
-      return { error: `Unable to load packfile ${filename}. It's suspiciously short - try deleting it; it may be corrupt.` }
-    }
-    const p = await GitPackIndex.fromPack({ pack, getExternalRefDelta })
-    // Save .idx file
-    fs.write(idxName, p.toBuffer())
-    return p
+    /* eslint-enable no-fallthrough */
   }
   static async hash ({ gitdir, type, object }) {
     return shasum(GitObject.wrap({ type, object }))
   }
   static async expandOid ({ fs: _fs, gitdir, oid: short }) {
     const fs = new FileSystem(_fs)
-    const prefix = short.slice(0, 2)
-
-    // Iterate through all the loose objects
-    const objectsSuffixes = await fs.readdir(`${gitdir}/objects/${prefix}`)
-    const results = objectsSuffixes
-      .map((suffix) => `${prefix}${suffix}`)
-      .filter((_oid) => _oid.startsWith(short))
-
-    // Iterate through all the packfiles as well
     // Curry the current read method so that the packfile un-deltification
     // process can acquire external ref-deltas.
     const getExternalRefDelta = oid =>
       GitObjectManager.read({ fs: _fs, gitdir, oid })
-    // Iterate through all the .pack files
-    let list = await fs.readdir(path.join(gitdir, '/objects/pack'))
-    list = list.filter(x => x.endsWith('.pack'))
-    for (let filename of list) {
-      // Try to get the packfile from the in-memory cache
-      let p = PackfileCache.get(filename)
-      const packFile = `${gitdir}/objects/pack/${filename}`
-      if (!p) {
-        p = GitObjectManager.loadPack(fs, packFile, getExternalRefDelta)
-        PackfileCache.set(filename, p)
-      }
-      p = await p
-      if (p.error) throw new GitError(E.InternalFail, { message: p.error })
-      // Search through the list of oids in the packfile
-      for (let oid of p.offsets.keys()) {
-        if (oid.startsWith(short)) results.push(oid)
-      }
-    }
+
+    const results1 = await GitObjectStoreLoose.expandOid({ fs, gitdir, oid: short })
+    const results2 = await GitObjectStorePacked.expandOid({ fs, gitdir, oid: short, getExternalRefDelta })
+    const results = results1.concat(results2)
 
     if (results.length === 1) {
       return results[0]
@@ -145,11 +91,7 @@ export class GitObjectManager {
       oid = shasum(object)
       object = Buffer.from(pako.deflate(object))
     }
-    let filepath = `${gitdir}/objects/${oid.slice(0, 2)}/${oid.slice(2)}`
-    // Don't overwrite existing git objects - this helps avoid EPERM errors.
-    // Although I don't know how we'd fix corrupted objects then. Perhaps delete them
-    // on read?
-    if (!await fs.exists(filepath)) await fs.write(filepath, object)
+    await GitObjectStoreLoose.write({ fs, gitdir, object, format: 'deflated', oid })
     return oid
   }
 }
---
+++ src/managers/GitObjectStorePacked.js
@@ -0,0 +1,87 @@
+import path from 'path'
+
+import { FileSystem } from '../models/FileSystem.js'
+import { E, GitError } from '../models/GitError.js'
+import { GitPackIndex } from '../models/GitPackIndex.js'
+
+const PackfileCache = new Map()
+
+export async function read ({ fs: _fs, gitdir, oid, format = 'content', getExternalRefDelta }) {
+  const fs = new FileSystem(_fs)
+  // Check to see if it's in a packfile.
+  // Iterate through all the .pack files
+  let list = await fs.readdir(path.join(gitdir, '/objects/pack'))
+  list = list.filter(x => x.endsWith('.pack'))
+  for (let filename of list) {
+    // Try to get the packfile from the in-memory cache
+    let p = PackfileCache.get(filename)
+    const packFile = `${gitdir}/objects/pack/${filename}`
+    if (!p) {
+      p = loadPack(fs, packFile, getExternalRefDelta)
+      PackfileCache.set(filename, p)
+    }
+    // If the packfile DOES have the oid we're looking for...
+    p = await p
+    if (p.error) throw new GitError(E.InternalFail, { message: p.error })
+    if (p.offsets.has(oid)) {
+      // Get the resolved git object from the packfile
+      if (!p.pack) {
+        p.pack = fs.read(packFile)
+      }
+      let result = await p.read({ oid, getExternalRefDelta })
+      result.format = 'content'
+      result.source = `objects/pack/${filename}`
+      return result
+    }
+  }
+  // Failed to find it
+  return null
+}
+
+export async function expandOid ({ fs: _fs, gitdir, oid: short, getExternalRefDelta }) {
+  const fs = new FileSystem(_fs)
+  // Iterate through all the .pack files
+  let results = []
+  let list = await fs.readdir(path.join(gitdir, '/objects/pack'))
+  list = list.filter(x => x.endsWith('.pack'))
+  for (let filename of list) {
+    // Try to get the packfile from the in-memory cache
+    let p = PackfileCache.get(filename)
+    const packFile = `${gitdir}/objects/pack/${filename}`
+    if (!p) {
+      p = loadPack(fs, packFile, getExternalRefDelta)
+      PackfileCache.set(filename, p)
+    }
+    p = await p
+    if (p.error) throw new GitError(E.InternalFail, { message: p.error })
+    // Search through the list of oids in the packfile
+    for (let oid of p.offsets.keys()) {
+      if (oid.startsWith(short)) results.push(oid)
+    }
+  }
+  return results
+}
+
+export async function write ({ fs: _fs, gitdir, type, object, format, oid }) {
+  // Not supported
+}
+
+
+async function loadPack (fs, filename, getExternalRefDelta) {
+  // If not there, load it from a .idx file
+  const idxName = filename.replace(/pack$/, 'idx')
+  if (await fs.exists(idxName)) {
+    const idx = await fs.read(idxName)
+    return GitPackIndex.fromIdx({ idx, getExternalRefDelta })
+  }
+  // If the .idx file isn't available, generate one.
+  const pack = await fs.read(filename)
+  // Sanity check. 12 byte header + 20 byte shasum
+  if (pack.length < 32) {
+    return { error: `Unable to load packfile ${filename}. It's suspiciously short - try deleting it; it may be corrupt.` }
+  }
+  const p = await GitPackIndex.fromPack({ pack, getExternalRefDelta })
+  // Save .idx file
+  fs.write(idxName, p.toBuffer())
+  return p
+}
