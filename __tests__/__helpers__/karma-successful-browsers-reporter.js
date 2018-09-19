442bac0bd5cb28efd463b4e11b541ec18bafd952
William Hilton <wmhilton@gmail.com>
2018-09-19 01:39:19 -0400

    save the full name of each browser tested with to dist/successful-browser-tests.json

--- __tests__/__helpers__/karma-successful-browsers-reporter.js
+++ __tests__/__helpers__/karma-successful-browsers-reporter.js
@@ -1,5 +1,4 @@
-const fs = require('fs')
-const path = require('path')
+const loadSuccessfulBrowsers = require('./karma-load-successful-browsers.js')
 
 const BrowsersReporter = function (
   baseReporterDecorator,
@@ -10,23 +9,16 @@ const BrowsersReporter = function (
 ) {
   this.browserCount = 0
   this.buildOk = false
+  this.successfulBrowsersFullNames = []
   this.successfulBrowsers = []
   this.failedBrowsers = []
   this.onRunStart = function (browsers) {
     this.browserCount = browsers.length
     this.buildOk = true
     // Append to the existing list of successful browsers
-    try {
-      const browsers = JSON.parse(
-        fs.readFileSync(
-          path.join(process.cwd(), 'successful-browsers.json'),
-          'utf8'
-        )
-      )
-      this.successfulBrowsers = browsers
-    } catch (err) {
-      // nothing
-    }
+    let tmp = loadSuccessfulBrowsers.load()
+    this.successfulBrowsersFullNames = tmp[0]
+    this.successfulBrowsers = tmp[1]
   }
   this.onBrowserComplete = function (browser) {
     var results = browser.lastResult
@@ -34,29 +26,11 @@ const BrowsersReporter = function (
       this.buildOk = false
       this.failedBrowsers.push(browser)
     } else {
-      if (browser.name.startsWith('HeadlessChrome')) {
-        this.successfulBrowsers.push('ChromeHeadlessNoSandbox')
-      } else if (browser.name.startsWith('Firefox')) {
-        this.successfulBrowsers.push('FirefoxHeadless')
-      } else if (browser.name.startsWith('Edge')) {
-        this.successfulBrowsers.push('sl_edge')
-      } else if (browser.name.startsWith('Mobile Safari')) {
-        this.successfulBrowsers.push('sl_ios_safari')
-      } else if (browser.name.startsWith('Chrome Mobile')) {
-        this.successfulBrowsers.push('sl_android_chrome')
-      } else if (browser.name.startsWith('Safari')) {
-        this.successfulBrowsers.push('sl_safari')
-      } else {
-        console.log(JSON.stringify(browser, null, 2))
-      }
+      this.successfulBrowsersFullNames.push(browser.name)
     }
   }
   this.onRunComplete = function () {
-    fs.writeFileSync(
-      'successful-browsers.json',
-      JSON.stringify(this.successfulBrowsers, null, 2),
-      'utf8'
-    )
+    loadSuccessfulBrowsers.save(this.successfulBrowsersFullNames)
   }
 }
 
