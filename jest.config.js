228126275199ec68b1530af873475c92f49fbece
William Hilton <wmhilton@gmail.com>
2018-09-23 17:19:25 -0400

    ci: switch from Travis to Azure Pipelines (#471)

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  'testRegex': '/__tests__/[^\\/]+\\.js',
+  'moduleNameMapper': {
+    '^isomorphic-git$': '<rootDir>/src',
+    '^isomorphic-git/(.+)$': '<rootDir>/src/$1'
+  },
+  'collectCoverageFrom': [
+    'src/**'
+  ],
+  'testEnvironment': 'node',
+  'reporters': [
+    'default',
+    ['jest-junit', {
+      output: `./junit/TESTS-node-${process.version}-${process.platform}-${require('os').release()}.xml`
+    }]
+  ],
+  'coverageReporters': [
+    'lcov',
+    'cobertura'
+  ]
+}
