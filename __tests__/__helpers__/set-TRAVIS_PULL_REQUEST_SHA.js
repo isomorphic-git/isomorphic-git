7a292238102658c045a086f8f19755b48c354806
William Hilton <wmhilton@gmail.com>
2018-09-23 16:21:35 -0400

    further simplify polyfill

--- __tests__/__helpers__/set-TRAVIS_PULL_REQUEST_SHA.js
+++ __tests__/__helpers__/set-TRAVIS_PULL_REQUEST_SHA.js
@@ -1,18 +1,11 @@
-const {execSync} = require('child_process')
-const path = require('path')
+const { execSync } = require('child_process')
 
 if (!process.env.TRAVIS_PULL_REQUEST_SHA) {
-  // Azure
-  if (process.env.BUILD_REPOSITORY_LOCALPATH) {
-    // (Trying not to rely on isomorphic-git here since that's the thing being tested.)
-    let GIT_DIR = path.join(process.env.BUILD_REPOSITORY_LOCALPATH, '.git')
-    console.log('Hoping GIT_DIR is', GIT_DIR)
-    let parents = execSync('git log --pretty=%P -n 1', {
-      // env: { GIT_DIR },
-      encoding: 'utf8'
-    }).split(' ').map(x => x.trim())
-    // parents[0] === master branch
-    // parents[1] === pr branch
-    process.env.TRAVIS_PULL_REQUEST_SHA = parents[1]
-  }
+  // I'm trying not to rely on isomorphic-git here since that's the thing being tested.
+  let parents = execSync('git log --pretty=%P -n 1', { encoding: 'utf8' })
+    .split(' ')
+    .map(x => x.trim())
+  // parents[0] === master branch
+  // parents[1] === pr branch
+  process.env.TRAVIS_PULL_REQUEST_SHA = parents[1]
 }
