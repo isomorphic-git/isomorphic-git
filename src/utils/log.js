0b570512e322ef0879e0d783d915f5817050aa57
William Hilton <wmhilton@gmail.com>
2018-11-05 23:33:41 -0500

    shave a few kb

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,14 +1,12 @@
-import debug from 'debug'
-
-let _log = null
+let shouldLog = null
 
 export function log (...args) {
-  // Delay instantiating the logger until first use
-  // This is necessary to please the static analysis used to perform
-  // tree-shaking and dead code elimination.
-  if (!_log) {
-    _log = debug('isomorphic-git')
-    _log.log = console.log.bind(console)
+  if (shouldLog === null) {
+    shouldLog = process.env.DEBUG === '*' ||
+      process.env.DEBUG === 'isomorphic-git' ||
+      (typeof window !== 'undefined' && (window.localStorage.debug === '*' || window.localStorage.debug === 'isomorphic-git'))
   }
-  _log(...args)
+  if (shouldLog) {
+    console.log(...args)
+  }
 }
