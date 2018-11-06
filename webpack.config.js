171955a5b4ce465f61d8923868e36bc1c6241674
William Hilton <wmhilton@gmail.com>
2018-11-06 12:12:39 -0500

    fix: shave a good 10kb off the bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,19 @@ module.exports = [
         excludeAssets: 'internal\\.umd\\.min\\.js'
       }),
       new DuplicatePackageCheckerPlugin({
-        strict: false
+        strict: true
       })
     ],
+    resolve: {
+      alias: {
+        // Overwride the default 'stream' -> 'stream-browserify' mapping
+        'stream': require.resolve('readable-stream'),
+        // Override dependencies on readable-stream@2 with v3.
+        'readable-stream': require.resolve('readable-stream'),
+        // 'bops' depends on 0.0.2 but 1.x (used by node-libs-browser) is compatible
+        'base64-js': require.resolve('base64-js')
+      }
+    },
     module: {
       rules: [
         {
