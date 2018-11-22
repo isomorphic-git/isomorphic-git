97c500d87911e1911f7fff024f01ab8076dee622
William Hilton <wmhilton@gmail.com>
2018-11-22 13:59:34 -0500

    use package

--- karma.conf.js
+++ karma.conf.js
@@ -148,10 +148,6 @@ module.exports = function (config) {
             __dirname,
             'dist/bundle.umd.min.js'
             // 'src/index.js'
-          ),
-          '@isomorphic-git/pgp-plugin': path.resolve(
-            __dirname,
-            '__tests__/__helpers__/isomorphic-git-pgp-plugin.min.js'
           )
         }
       }
