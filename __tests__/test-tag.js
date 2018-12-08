4cf2f2229728ede44b23a7c8dfbd773b525be59a
Yu Shimura <mail@yuhr.org>
2018-12-09 06:57:06 +0900

    fix: unwanted behavior without 'force' option

--- __tests__/test-tag.js
+++ __tests__/test-tag.js
@@ -2,7 +2,7 @@
 const { makeFixture } = require('./__helpers__/FixtureFS.js')
 const snapshots = require('./__snapshots__/test-tag.js.snap')
 const registerSnapshots = require('./__helpers__/jasmine-snapshots')
-const { plugins, tag, resolveRef, writeObject } = require('isomorphic-git')
+const { plugins, tag, resolveRef } = require('isomorphic-git')
 
 describe('tag', () => {
   beforeAll(() => {
@@ -20,7 +20,7 @@ describe('tag', () => {
     const ref = await resolveRef({ gitdir, ref: 'refs/tags/latest' })
     expect(ref).toMatchSnapshot()
   })
-  it('doesn\'t fail on overwrite with the same value', async () => {
+  it('fails on overwrite', async () => {
     // Setup
     let { fs, gitdir } = await makeFixture('test-tag')
     plugins.set('fs', fs)
@@ -32,25 +32,6 @@ describe('tag', () => {
     } catch (err) {
       errorName = err.name
     }
-    expect(errorName).toBe(undefined)
-  })
-  it('fails on overwrite with a different value', async () => {
-    // Setup
-    let { fs, gitdir } = await makeFixture('test-tag')
-    plugins.set('fs', fs)
-    // Test
-    await tag({ gitdir, name: 'latest' })
-    const anotherOid = await writeObject({
-      gitdir,
-      type: 'blob',
-      object: Buffer.from('hello', 'utf8')
-    })
-    let errorName
-    try {
-      await tag({ gitdir, name: 'latest', value: anotherOid })
-    } catch (err) {
-      errorName = err.name
-    }
     expect(errorName).toBe('RefExistsError')
   })
   it('force overwrite', async () => {
@@ -59,14 +40,9 @@ describe('tag', () => {
     plugins.set('fs', fs)
     // Test
     await tag({ gitdir, name: 'latest' })
-    const anotherOid = await writeObject({
-      gitdir,
-      type: 'blob',
-      object: Buffer.from('hello', 'utf8')
-    })
     let errorName
     try {
-      await tag({ gitdir, name: 'latest', value: anotherOid, force: true })
+      await tag({ gitdir, name: 'latest', force: true })
     } catch (err) {
       errorName = err.name
     }
--- src/commands/tag.js
+++ src/commands/tag.js
@@ -29,25 +29,10 @@ export async function tag ({
       ref: value || 'HEAD'
     })
 
-    try {
-      // Attempt to get old value
-      const oldValue = await GitRefManager.resolve({ fs, gitdir, ref })
-      // If found and not forced overwriting
-      if (!force) {
-        if (oldValue !== value) {
-          // It exists and differ from passed value
-          throw new GitError(E.RefExistsError, { noun: 'tag', ref: name })
-        } else {
-          // Nothing to do, as the old value and the new one is the same
-          return
-        }
-      }
-    } catch (err) {
-      switch (err.name) {
-        case E.ResolveRefError:
-          // Not found, it's pretty OK
-          break
-        default: throw err
+    if (!force) {
+      const exist = await fs.exists(`${gitdir}/${ref}`)
+      if (exist) {
+        throw new GitError(E.RefExistsError, { noun: 'tag', ref: name })
       }
     }
 
