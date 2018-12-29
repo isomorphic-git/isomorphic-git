f7df9dec41269428c25a3e661aed9a333d44fa95
William Hilton <wmhilton@gmail.com>
2018-12-29 00:10:35 -0500

    fix: should work in Node 8.x now

--- src/utils/AsyncIterator.js
+++ src/utils/AsyncIterator.js
@@ -25,44 +25,51 @@ export function fromBuffer(buffer) {
 export function fromNodeStream(stream) {
   // Use native async iteration if it's available.
   if (stream[Symbol.asyncIterator]) return stream
-  stream.pause()
+  // Author's Note
+  // I tried many MANY ways to do this.
+  // I tried two npm modules (stream-to-async-iterator and streams-to-async-iterator) with no luck.
+  // I tried using 'readable' and .read(), and .pause() and .resume()
+  // It took me two loooong evenings to get to this point.
+  // So if you are horrified that this solution just builds up a queue with no backpressure,
+  // and turns Promises inside out, too bad. This is the first code that worked reliably.
   let ended = false
+  let queue = []
+  let defer = {}
+  stream.on('data', chunk => {
+    queue.push(chunk)
+    if (defer.resolve) {
+      defer.resolve({value: queue.shift(), done: false})
+      defer = {}
+    }
+  })
+  stream.on('error', err => {
+    if (defer.reject) {
+      defer.reject(err)
+      defer = {}
+    }
+  })
+  stream.on('end', () => {
+    ended = true
+    if (defer.resolve) {
+      defer.resolve({done: true})
+      defer = {}
+    }
+  })
   return {
     next() {
       return new Promise((resolve, reject) => {
-        if (ended) return resolve({done: true})
-        stream.once('error', err => {
-          stream.removeAllListeners()
-          reject(err)
-        })
-        stream.once('end', () => {
-          stream.removeAllListeners()
-          ended = true
-          resolve({done: true})
-        })
-        stream.once('readable', () => {
-          stream.pause()
-          stream.removeAllListeners()
-          let buffers = []
-          let data = stream.read()
-          while (data) {
-            buffers.push(data)
-            data = stream.read()
-          }
-          let nextbuffer = Buffer.concat(buffers)
-          if (nextbuffer.length === 0) {
-            ended = true
-            stream.destroy()
-          }
-          resolve({done: false, value: nextbuffer})
-        })
-        stream.resume()
+        if (queue.length === 0 && ended) {
+          return resolve({done: true})
+        } else if (queue.length > 0) {
+          return resolve({value: queue.shift(), done: false})
+        } else if (queue.length === 0 && !ended) {
+          defer = {resolve, reject}
+        }
       })
     },
     return() {
-      stream.pause()
       stream.removeAllListeners()
-      return {}
+      if (stream.destroy) stream.destroy()
     },
     [Symbol.asyncIterator]() { return this }
   }
