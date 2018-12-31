e4483920489f9246fd8452c55733881f59a5acc9
William Hilton <wmhilton@gmail.com>
2018-12-30 21:08:06 -0500

    format prettier-standard

--- src/utils/http.js
+++ src/utils/http.js
@@ -36,12 +36,7 @@ async function httpBrowser ({ url, method = 'GET', headers = {}, body }) {
   }
 }
 
-async function httpNode ({
-  url,
-  method = 'GET',
-  headers = {},
-  body
-}) {
+async function httpNode ({ url, method = 'GET', headers = {}, body }) {
   // If we can, we should send it as a single buffer so it sets a Content-Length header.
   if (body && Array.isArray(body)) {
     body = await collect(body)
@@ -50,22 +45,25 @@ async function httpNode ({
   }
   return new Promise((resolve, reject) => {
     const get = require('simple-get')
-    get({
-      url,
-      method,
-      headers,
-      body
-    }, (err, res) => {
-      if (err) return reject(err)
-      let iter = fromNodeStream(res)
-      resolve({
-        url: res.url,
-        method: res.method,
-        statusCode: res.statusCode,
-        statusMessage: res.statusMessage,
-        body: iter,
-        headers: res.headers
-      })
-    })
+    get(
+      {
+        url,
+        method,
+        headers,
+        body
+      },
+      (err, res) => {
+        if (err) return reject(err)
+        let iter = fromNodeStream(res)
+        resolve({
+          url: res.url,
+          method: res.method,
+          statusCode: res.statusCode,
+          statusMessage: res.statusMessage,
+          body: iter,
+          headers: res.headers
+        })
+      }
+    )
   })
 }
