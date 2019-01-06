4bcab20e33cd7c82373b3b124348e7c25d65a9c3
Dan Lynch <pyramation@gmail.com>
2019-01-06 19:12:13 -0300

    feat: 'add' command accepts directories and respects .gitignore (#627)

--- __tests__/test-add.js
+++ __tests__/test-add.js
@@ -3,6 +3,12 @@ const { makeFixture } = require('./__helpers__/FixtureFS.js')
 
 const { init, add, listFiles } = require('isomorphic-git')
 
+// NOTE: we cannot actually commit a real .gitignore file in fixtures or fixtures won't be included in this repo
+const writeGitIgnore = async (fs, dir) => fs.write(
+  dir + '/.gitignore',
+  ['*-pattern.js', 'i.txt', 'js_modules', '.DS_Store'].join('\n')
+)
+
 describe('add', () => {
   it('file', async () => {
     // Setup
@@ -10,13 +16,22 @@ describe('add', () => {
     // Test
     await init({ dir })
     await add({ dir, filepath: 'a.txt' })
-    expect((await listFiles({ dir })).length === 1).toBe(true)
+    expect((await listFiles({ dir })).length).toEqual(1)
     await add({ dir, filepath: 'a.txt' })
-    expect((await listFiles({ dir })).length === 1).toBe(true)
+    expect((await listFiles({ dir })).length).toEqual(1)
     await add({ dir, filepath: 'a-copy.txt' })
-    expect((await listFiles({ dir })).length === 2).toBe(true)
+    expect((await listFiles({ dir })).length).toEqual(2)
     await add({ dir, filepath: 'b.txt' })
-    expect((await listFiles({ dir })).length === 3).toBe(true)
+    expect((await listFiles({ dir })).length).toEqual(3)
+  })
+  it('ignored file', async () => {
+    // Setup
+    let { fs, dir } = await makeFixture('test-add')
+    await writeGitIgnore(fs, dir)
+    // Test
+    await init({ dir })
+    await add({ dir, filepath: 'i.txt' })
+    expect((await listFiles({ dir })).length).toEqual(0)
   })
   it('non-existant file', async () => {
     // Setup
@@ -30,5 +45,34 @@ describe('add', () => {
       err = e
     }
     expect(err.caller).toEqual('git.add')
+  })
+  it('folder', async () => {
+    // Setup
+    let { dir } = await makeFixture('test-add')
+    // Test
+    await init({ dir })
+    expect((await listFiles({ dir })).length).toEqual(0)
+    await add({ dir, filepath: 'c' })
+    expect((await listFiles({ dir })).length).toEqual(4)
+  })
+  it('folder with .gitignore', async () => {
+    // Setup
+    let { fs, dir } = await makeFixture('test-add')
+    await writeGitIgnore(fs, dir)
+    // Test
+    await init({ dir })
+    expect((await listFiles({ dir })).length).toEqual(0)
+    await add({ dir, filepath: 'c' })
+    expect((await listFiles({ dir })).length).toEqual(3)
+  })
+  it('git add .', async () => {
+    // Setup
+    let { fs, dir } = await makeFixture('test-add')
+    await writeGitIgnore(fs, dir)
+    // Test
+    await init({ dir })
+    expect((await listFiles({ dir })).length).toEqual(0)
+    await add({ dir, filepath: '.' })
+    expect((await listFiles({ dir })).length).toEqual(7)
   })
 })
