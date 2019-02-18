89be615e60186cfd11548f653077ca82abb0f584
William Hilton <wmhilton@gmail.com>
2019-02-18 14:30:56 -0500

    fix: join('/', '.git') should not return '//.git' anymore (#699)

---
+++ src/utils/normalizePath.js
@@ -0,0 +1,11 @@
+export function normalizePath (path) {
+  return path
+    .replace(/\/\.\//g, '/') // Replace '/./' with '/'
+    .replace(/\/{2,}/g, '/') // Replace consecutive '/'
+    .replace(/^\/\.$/, '/') // if path === '/.' return '/'
+    .replace(/^\.\/$/, '.') // if path === './' return '.'
+    .replace(/^\.\//, '') // Remove leading './'
+    .replace(/\/\.$/, '') // Remove trailing '/.'
+    .replace(/(.+)\/$/, '$1') // Remove trailing '/'
+    .replace(/^$/, '.') // if path === '' return '.'
+}
