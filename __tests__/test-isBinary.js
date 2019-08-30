fe2cb4e84cce80337d7d5d5bcd137105eb34b473
William Hilton <wmhilton@gmail.com>
2019-08-30 16:29:28 -0400

    chore(merge): implement isBinary util (#831)

---
+++ __tests__/test-isBinary.js
@@ -0,0 +1,35 @@
+/* eslint-env node, browser, jasmine */
+const { makeFixture } = require('./__helpers__/FixtureFS.js')
+const path = require('path')
+
+const { isBinary } = require('isomorphic-git/internal-apis')
+
+const binaryFiles = [
+  'browserconfig.gz',
+  'browserconfig.zip',
+  'favicon-16x16.gif',
+  'favicon-16x16.png'
+]
+const textFiles = ['browserconfig.xml', 'manifest.json']
+
+describe('isBinary', () => {
+  for (const file of binaryFiles) {
+    it(`${path.extname(file)} is binary`, async () => {
+      // Setup
+      const { fs, dir } = await makeFixture('test-isBinary')
+      const buffer = await fs.read(`${dir}/${file}`)
+      // Test
+      expect(isBinary(buffer)).toEqual(true)
+    })
+  }
+
+  for (const file of textFiles) {
+    it(`${path.extname(file)} is NOT binary`, async () => {
+      // Setup
+      const { fs, dir } = await makeFixture('test-isBinary')
+      const buffer = await fs.read(`${dir}/${file}`)
+      // Test
+      expect(isBinary(buffer)).toEqual(false)
+    })
+  }
+})
