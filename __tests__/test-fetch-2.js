28290ab044ea3224de2c3255aa5b42625a973565
William Hilton <wmhilton@gmail.com>
2019-09-07 21:20:38 -0400

    totally flailing

--- __tests__/test-fetch-2.js
+++ __tests__/test-fetch-2.js
@@ -1,9 +1,61 @@
 /* eslint-env node, browser, jasmine */
 const { makeFixture } = require('./__helpers__/FixtureFS.js')
 
-const { fetch } = require('isomorphic-git')
+const { E, fetch } = require('isomorphic-git')
 
 describe('fetch (part 2)', () => {
+  it('errors if missing refspec', async () => {
+    const { gitdir } = await makeFixture('test-issue-84')
+    // Test
+    let err = null
+    try {
+      await fetch({
+        gitdir,
+        since: new Date(1506571200000),
+        singleBranch: true,
+        remote: 'origin',
+        ref: 'test-branch-shallow-clone'
+      })
+    } catch (e) {
+      err = e
+    }
+    expect(err).toBeDefined()
+    expect(err.code).toEqual(E.NoRefspecConfiguredError)
+  })
+
+  it('fetch empty repository from git-http-mock-server', async () => {
+    const { fs, dir, gitdir } = await makeFixture('test-empty')
+    await fetch({
+      dir,
+      gitdir,
+      depth: 1,
+      url: 'http://localhost:8888/test-empty.git'
+    })
+    expect(await fs.exists(`${dir}`)).toBe(true)
+    expect(await fs.exists(`${gitdir}/HEAD`)).toBe(true)
+    expect((await fs.read(`${gitdir}/HEAD`)).toString('utf-8').trim()).toEqual(
+      'ref: refs/heads/master'
+    )
+    expect(await fs.exists(`${gitdir}/refs/heads/master`)).toBe(false)
+  })
+
+  it('fetch --prune from git-http-mock-server', async () => {
+    const { fs, dir, gitdir } = await makeFixture('test-fetch-client')
+    expect(await fs.exists(`${gitdir}/refs/remotes/origin/test-prune`)).toBe(
+      true
+    )
+    const { pruned } = await fetch({
+      dir,
+      gitdir,
+      depth: 1,
+      prune: true
+    })
+    expect(pruned).toEqual(['refs/remotes/origin/test-prune'])
+    expect(await fs.exists(`${gitdir}/refs/remotes/origin/test-prune`)).toBe(
+      false
+    )
+  })
+
   it('fetch --prune-tags from git-http-mock-server', async () => {
     const { fs, dir, gitdir } = await makeFixture('test-fetch-client')
     expect(await fs.exists(`${gitdir}/refs/tags/v1.0.0-beta1`)).toBe(true)
--- __tests__/test-fetch.js
+++ __tests__/test-fetch.js
@@ -3,7 +3,7 @@ const { makeFixture } = require('./__helpers__/FixtureFS.js')
 
 const EventEmitter = require('events')
 const { sleep } = require('isomorphic-git/internal-apis')
-const { E, plugins, fetch } = require('isomorphic-git')
+const { plugins, fetch } = require('isomorphic-git')
 
 describe('fetch', () => {
   it('fetch (from Github)', async () => {
@@ -114,56 +114,5 @@ describe('fetch', () => {
     await sleep(1000) // seems to be a problem spot
     shallow = (await fs.read(`${gitdir}/shallow`)).toString('utf8')
     expect(shallow).toEqual('86ec153c7b48e02f92930d07542680f60d104d31\n')
-  })
-
-  it('errors if missing refspec', async () => {
-    const { gitdir } = await makeFixture('test-issue-84')
-    // Test
-    let err = null
-    try {
-      await fetch({
-        gitdir,
-        since: new Date(1506571200000),
-        singleBranch: true,
-        remote: 'origin',
-        ref: 'test-branch-shallow-clone'
-      })
-    } catch (e) {
-      err = e
-    }
-    expect(err).toBeDefined()
-    expect(err.code).toEqual(E.NoRefspecConfiguredError)
-  })
-  it('fetch empty repository from git-http-mock-server', async () => {
-    const { fs, dir, gitdir } = await makeFixture('test-empty')
-    await fetch({
-      dir,
-      gitdir,
-      depth: 1,
-      url: 'http://localhost:8888/test-empty.git'
-    })
-    expect(await fs.exists(`${dir}`)).toBe(true)
-    expect(await fs.exists(`${gitdir}/HEAD`)).toBe(true)
-    expect((await fs.read(`${gitdir}/HEAD`)).toString('utf-8').trim()).toEqual(
-      'ref: refs/heads/master'
-    )
-    expect(await fs.exists(`${gitdir}/refs/heads/master`)).toBe(false)
-  })
-
-  it('fetch --prune from git-http-mock-server', async () => {
-    const { fs, dir, gitdir } = await makeFixture('test-fetch-client')
-    expect(await fs.exists(`${gitdir}/refs/remotes/origin/test-prune`)).toBe(
-      true
-    )
-    const { pruned } = await fetch({
-      dir,
-      gitdir,
-      depth: 1,
-      prune: true
-    })
-    expect(pruned).toEqual(['refs/remotes/origin/test-prune'])
-    expect(await fs.exists(`${gitdir}/refs/remotes/origin/test-prune`)).toBe(
-      false
-    )
   })
 })
