5ec43a317ee3ffca4ae437a471d14503628e49ee
William Hilton <wmhilton@gmail.com>
2019-09-07 23:21:22 -0400

    what if JUST assertions are commented out?

--- __tests__/test-fetch.js
+++ __tests__/test-fetch.js
@@ -168,21 +168,23 @@ describe('fetch', () => {
     )
   })
 
-  // it('fetch --prune-tags from git-http-mock-server', async () => {
-  //   const { fs, dir, gitdir } = await makeFixture('test-fetch-client')
-  //   expect(await fs.exists(`${gitdir}/refs/tags/v1.0.0-beta1`)).toBe(true)
-  //   const oldValue = await fs.read(`${gitdir}/refs/tags/v1.0.0`, 'utf8')
-  //   await fetch({
-  //     dir,
-  //     gitdir,
-  //     depth: 1,
-  //     tags: true,
-  //     pruneTags: true
-  //   })
+  it('fetch --prune-tags from git-http-mock-server', async () => {
+    const { fs, dir, gitdir } = await makeFixture('test-fetch-client')
+    //   expect(await fs.exists(`${gitdir}/refs/tags/v1.0.0-beta1`)).toBe(true)
+    //   const oldValue = await fs.read(`${gitdir}/refs/tags/v1.0.0`, 'utf8')
+    await fetch({
+      fs,
+      dir,
+      gitdir,
+      depth: 1,
+      tags: true,
+      pruneTags: true
+    })
+    expect(true).toBe(true)
   //   // assert that tag was deleted
   //   expect(await fs.exists(`${gitdir}/refs/tags/v1.0.0-beta1`)).toBe(false)
   //   // assert that tags was force-updated
   //   const newValue = await fs.read(`${gitdir}/refs/tags/v1.0.0`, 'utf8')
   //   expect(oldValue).not.toEqual(newValue)
-  // })
+  })
 })
