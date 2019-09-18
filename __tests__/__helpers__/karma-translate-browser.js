55f6b16eff8a195d6fb6899245f40d2dd1029372
William Hilton <wmhilton@gmail.com>
2019-09-18 15:50:28 -0400

    wtf prettier

--- __tests__/__helpers__/karma-translate-browser.js
+++ __tests__/__helpers__/karma-translate-browser.js
@@ -1,7 +1,6 @@
 module.exports = function translateBrowser (fullname) {
-  if (fullname.startsWith('X ')) {
-
-  } else if (fullname.startsWith('HeadlessChrome')) {
+  if (fullname.startsWith('X ')) return
+  if (fullname.startsWith('HeadlessChrome')) {
     return 'ChromeHeadlessNoSandbox'
   } else if (fullname.startsWith('Firefox')) {
     return 'FirefoxHeadless'
