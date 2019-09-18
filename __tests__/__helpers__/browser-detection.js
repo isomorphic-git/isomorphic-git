0b29576d3716016ee2a82ee407adb1b60787c548
William Hilton <wmhilton@gmail.com>
2019-09-18 16:10:33 -0400

    doh

--- __tests__/__helpers__/browser-detection.js
+++ __tests__/__helpers__/browser-detection.js
@@ -1,3 +1,3 @@
 /* eslint-env node, browser, jasmine */
 
-export const isSafariMobile11 = process.browser && /Version\/11\.*Safari.*Mobile/.test(navigator && navigator.userAgent)
+export const isSafariMobile11 = process.browser && /Version\/11\.*Mobile.*Safari/.test(navigator && navigator.userAgent)
