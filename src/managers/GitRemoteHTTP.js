5ea8c03d7d0f66a371110b98755e613f6c814bb1
William Hilton <wmhilton@gmail.com>
2019-11-13 22:38:48 -0500

    const

--- src/managers/GitRemoteHTTP.js
+++ src/managers/GitRemoteHTTP.js
@@ -114,9 +114,9 @@ export class GitRemoteHTTP {
       // If they don't send the correct content-type header, that's a good indicator it is either a "dumb" HTTP
       // server, or the user specified an incorrect remote URL and the response is actually an HTML page.
       // In this case, we save the response as plain text so we can generate a better error message if needed.
-      let data = await collect(res.body)
-      let response = data.toString('utf8')
-      let preview =
+      const data = await collect(res.body)
+      const response = data.toString('utf8')
+      const preview =
         response.length < 256 ? response : response.slice(0, 256) + '...'
       // For backwards compatibility, try to parse it anyway.
       try {
