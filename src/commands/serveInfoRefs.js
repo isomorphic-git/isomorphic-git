1e51ec05d4651e6f3db295d48632b97724232da5
William Hilton <wmhilton@gmail.com>
2019-11-28 00:59:02 -0500

    wip

--- src/commands/serveInfoRefs.js
+++ src/commands/serveInfoRefs.js
@@ -4,6 +4,10 @@ import { join } from '../utils/join.js'
 import { cores } from '../utils/plugins.js'
 import { writeRefsAdResponse } from '../wire/writeRefsAdResponse.js'
 
+/**
+ * @param {object} args
+ * @param {'git-upload-pack' | 'git-receive-pack'} args.service
+ */
 export async function serveInfoRefs ({
   core = 'default',
   dir,
@@ -15,42 +19,71 @@ export async function serveInfoRefs ({
   try {
     // Send a refs advertisement
     const capabilities = [
-      'thin-pack',
-      'side-band',
-      'side-band-64k',
-      'shallow',
-      'deepen-since',
-      'deepen-not',
-      'allow-tip-sha1-in-want',
-      'allow-reachable-sha1-in-want'
+      'ofs-delta',
+      'side-band-64k'
     ]
+    if (service === 'git-upload-pack') {
+      capabilities.push(
+        'no-done',
+        'no-thin',
+        // 'multi_ack',
+        // 'multi_ack_detailed',
+        // 'thin-pack',
+        // 'shallow',
+        // 'deepen-since',
+        // 'deepen-not',
+        // 'deepen-relative',
+        // 'no-progress',
+        // 'include-tag',
+        // 'filter',
+      )
+    } else if (service === 'git-receive-pack') {
+      capabilities.push(
+        'report-status',
+        'delete-refs',
+        // 'allow-tip-sha1-in-want',
+        // 'allow-reachable-sha1-in-want',
+        // 'quiet',
+        // 'atomic',
+        // 'push-options',
+        // 'push-cert',
+      )
+    }
+
+    const refs = {}
+    const symrefs = {}
+
+    // Only sent for upload-pack apparently?
+    if (service === 'git-upload-pack') {
+      try {
+        symrefs['HEAD'] = await GitRefManager.resolve({
+          fs,
+          gitdir,
+          ref: 'HEAD',
+          depth: 2
+        })
+      } catch (e) {
+        // noop
+      }
+      try {
+        // Note: HEAD must be the first in the list.
+        refs['HEAD'] = await GitRefManager.resolve({ fs, gitdir, ref: 'HEAD' })
+      } catch (e) {
+        // noop
+      }
+    }
+
     let keys = await GitRefManager.listRefs({
       fs,
       gitdir,
       filepath: 'refs'
     })
     keys = keys.map(ref => `refs/${ref}`)
-    const refs = {}
-    // HEAD must be the first in the list
-    try {
-      refs['HEAD'] = await GitRefManager.resolve({ fs, gitdir, ref: 'HEAD' })
-    } catch (e) {
-      // noop
-    }
+    // Note: be sure to set HEAD before these other refs (if git-upload-pack)
     for (const key of keys) {
       refs[key] = await GitRefManager.resolve({ fs, gitdir, ref: key })
     }
-    const symrefs = {}
-    try {
-      symrefs['HEAD'] = await GitRefManager.resolve({
-        fs,
-        gitdir,
-        ref: 'HEAD',
-        depth: 2
-      })
-    } catch (e) {
-      // noop
-    }
+
     const response = await writeRefsAdResponse({
       service,
       capabilities,
@@ -59,10 +92,8 @@ export async function serveInfoRefs ({
     })
     return {
       headers: {
-        'content-type': `application/x-${service}-advertisement`,
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache, max-age=0, must-revalidate',
-        'vary': 'Accept-Encoding',
+        'Content-Type': `application/x-${service}-advertisement`,
+        'Cache-Control': 'no-cache, max-age=0, must-revalidate',
       },
       response
     }
--- src/commands/serveReceivePack.js
+++ src/commands/serveReceivePack.js
@@ -17,17 +17,17 @@ export async function serveReceivePack ({
   const fs = new FileSystem(_fs)
   try {
     const response = []
+    response.push(GitPktLine.encodeSideBand(GitSideBand.MessageChannel, 'Resolving deltas:   0% (0/4)\n'))
     response.push(GitPktLine.encodeSideBand(GitSideBand.PackfileChannel, GitPktLine.encode('unpack ok\n')))
     response.push(GitPktLine.encodeSideBand(GitSideBand.PackfileChannel, GitPktLine.encode('ok refs/heads/master\n')))
+    response.push(GitPktLine.encodeSideBand(GitSideBand.MessageChannel, banner))
     response.push(GitPktLine.encodeSideBand(GitSideBand.PackfileChannel, GitPktLine.flush()))
     response.push(GitPktLine.flush())
 
     return {
       headers: {
         'Content-Type': `application/x-${service}-result`,
-        'Pragma': 'no-cache',
         'Cache-Control': 'no-cache, max-age=0, must-revalidate',
-        'Vary': 'Accept-Encoding',
       },
       response
     }
