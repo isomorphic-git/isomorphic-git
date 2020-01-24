d931137c410a4cec798d1b284d26cd8fab14b92a
William Hilton <wmhilton@gmail.com>
2020-01-23 23:33:21 -0500

    chore: cleanup test output

--- karma.conf.js
+++ karma.conf.js
@@ -136,11 +136,10 @@ module.exports = function (config) {
     singleRun: true,
     // test results reporter to use
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['browsers', 'verbose', 'longest', 'pr-comment', 'junit'],
+    reporters: ['browsers', 'verbose', 'pr-comment', 'junit'],
     junitReporter: {
       outputDir: './junit'
     },
-    longestSpecsToReport: 50,
     browserify: {
       transform: [
         // Replace process.env.CI
@@ -180,7 +179,6 @@ module.exports = function (config) {
       'karma-firefox-launcher',
       'karma-jasmine',
       'karma-junit-reporter',
-      'karma-longest-reporter',
       'karma-sauce-launcher',
       'karma-verbose-reporter',
       'karma-webpack',
