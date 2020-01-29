a88af5c522b1bff093340b5f5021bb54e37ac06c
William Hilton <wmhilton@gmail.com>
2020-01-28 22:11:07 -0500

    chore(ci): fix bundle size check (#1014)

--- __tests__/__helpers__/set-TRAVIS_PULL_REQUEST_SHA.js
+++ __tests__/__helpers__/set-TRAVIS_PULL_REQUEST_SHA.js
@@ -2,9 +2,8 @@ const { execSync } = require('child_process')
 
 if (!process.env.TRAVIS_PULL_REQUEST_SHA) {
   // I'm trying not to rely on isomorphic-git here since that's the thing being tested.
-  const parents = execSync('git log --pretty=%P -n 1', { encoding: 'utf8' })
-    .split(' ')
-    .map(x => x.trim())
+  const log = execSync('git log --pretty=%P -n 1', { encoding: 'utf8' })
+  const parents = log.split(' ').map(x => x.trim())
   // parents[0] === master branch
   // parents[1] === pr branch
   process.env.TRAVIS_PULL_REQUEST_SHA = parents[1]
