a4e5aeeb9eccdd19dbd491b80577f3596cb04c31
William Hilton <wmhilton@gmail.com>
2020-02-08 16:03:19 -0500

    fix(fetch, push): recognize more git config settings (#1033)
    BREAKING CHANGE: the `fetch` and `push` functions now respect previously ignored git config settings like: `branch.${ref}.pushRemote`, `remote.pushDefault`, `remote.${remote}.pushurl`, and `branch.${ref}.merge`. (Note the `pull` command already respected `branch.${ref}.merge` but `fetch` did not.)

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -21,10 +21,6 @@ import { join } from '../utils/join.js'
 /**
  * Fetch commits from a remote repository
  *
- * Future versions of isomorphic-git might return additional metadata.
- *
- * To monitor progress events, see the documentation for the [`'emitter'` plugin](./plugin_emitter.md).
- *
  * @param {object} args
  * @param {FsClient} args.fs - a file system client
  * @param {HttpClient} args.http - an HTTP client
@@ -35,24 +31,25 @@ import { join } from '../utils/join.js'
  * @param {AuthFailureCallback} [args.onAuthFailure] - optional auth rejected callback
  * @param {string} [args.dir] - The [working tree](dir-vs-gitdir.md) directory path
  * @param {string} [args.gitdir=join(dir,'.git')] - [required] The [git directory](dir-vs-gitdir.md) path
- * @param {string} [args.url] - The URL of the remote repository. Will be gotten from gitconfig if absent.
- * @param {string} [args.corsProxy] - Optional [CORS proxy](https://www.npmjs.com/%40isomorphic-git/cors-proxy). Overrides value in repo config.
- * @param {string} [args.ref = 'HEAD'] - Which branch to fetch. By default this is the currently checked out branch.
+ * @param {string} [args.ref] - Which branch to fetch. By default this is the currently checked out branch.
+ * @param {string} [args.url] - The URL of the remote repository. The default is the value set in the git config for that remote.
+ * @param {string} [args.remote] - If URL is not specified, determines which remote to use.
+ * @param {string} [args.remoteRef] - The name of the branch on the remote to fetch. By default this is the configured remote tracking branch.
  * @param {boolean} [args.singleBranch = false] - Instead of the default behavior of fetching all the branches, only fetch a single branch.
- * @param {boolean} [args.noGitSuffix = false] - If true, clone will not auto-append a `.git` suffix to the `url`. (**AWS CodeCommit needs this option**)
  * @param {boolean} [args.tags = false] - Also fetch tags
- * @param {string} [args.remote] - What to name the remote that is created.
  * @param {number} [args.depth] - Integer. Determines how much of the git repository's history to retrieve
+ * @param {boolean} [args.relative = false] - Changes the meaning of `depth` to be measured from the current shallow depth rather than from the branch tip.
  * @param {Date} [args.since] - Only fetch commits created after the given date. Mutually exclusive with `depth`.
  * @param {string[]} [args.exclude = []] - A list of branches or tags. Instructs the remote server not to send us any commits reachable from these refs.
- * @param {boolean} [args.relative = false] - Changes the meaning of `depth` to be measured from the current shallow depth rather than from the branch tip.
+ * @param {boolean} [args.prune] - Delete local remote-tracking branches that are not present on the remote
+ * @param {boolean} [args.pruneTags] - Prune local tags that don’t exist on the remote, and force-update those tags that differ
+ * @param {boolean} [args.noGitSuffix = false] - If true, clone will not auto-append a `.git` suffix to the `url`. (**AWS CodeCommit needs this option**)
+ * @param {string} [args.corsProxy] - Optional [CORS proxy](https://www.npmjs.com/%40isomorphic-git/cors-proxy). Overrides value in repo config.
  * @param {string} [args.username] - See the [Authentication](./authentication.html) documentation
  * @param {string} [args.password] - See the [Authentication](./authentication.html) documentation
  * @param {string} [args.token] - See the [Authentication](./authentication.html) documentation
  * @param {'github' | 'bitbucket' | 'gitlab'} [args.oauth2format] - See the [Authentication](./authentication.html) documentation
  * @param {Object<string, string>} [args.headers] - Additional headers to include in HTTP requests, similar to git's `extraHeader` config
- * @param {boolean} [args.prune] - Delete local remote-tracking branches that are not present on the remote
- * @param {boolean} [args.pruneTags] - Prune local tags that don’t exist on the remote, and force-update those tags that differ
  *
  * @returns {Promise<FetchResult>} Resolves successfully when fetch completes
  * @see FetchResult
@@ -81,9 +78,8 @@ export async function fetch ({
   dir,
   gitdir = join(dir, '.git'),
   ref = 'HEAD',
-  // @ts-ignore
-  refs = [ref],
   remote,
+  remoteRef,
   url,
   noGitSuffix = false,
   corsProxy,
@@ -125,8 +121,8 @@ export async function fetch ({
       onAuthFailure,
       gitdir,
       ref,
-      refs,
       remote,
+      remoteRef,
       url,
       noGitSuffix,
       corsProxy,
