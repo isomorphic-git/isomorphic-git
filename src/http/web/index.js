41d9031e80748fe032ed3012881d0ac0f67f60b6
William Hilton <wmhilton@gmail.com>
2020-02-20 13:42:48 -0500

    fix .gitignore file haha

---
+++ src/http/web/index.js
@@ -0,0 +1,66 @@
+/* eslint-env browser */
+import { collect } from '../../utils/collect.js'
+import { fromStream } from '../../utils/fromStream'
+
+// Sorry for the copy & paste from typedefs.js but if we import typedefs.js we'll get a whole bunch of extra comments
+// in the rollup output
+
+/**
+ * @typedef {Object} GitHttpRequest
+ * @property {string} url - The URL to request
+ * @property {string} [method='GET'] - The HTTP method to use
+ * @property {Object<string, string>} [headers={}] - Headers to include in the HTTP request
+ * @property {AsyncIterableIterator<Uint8Array>} [body] - An async iterator of Uint8Arrays that make up the body of POST requests
+ * @property {string} [core] - If your `http` plugin needs access to other plugins, it can do so via `git.cores.get(core)`
+ * @property {GitEmitterPlugin} [emitter] - If your `http` plugin emits events, it can do so via `emitter.emit()`
+ * @property {string} [emitterPrefix] - The `emitterPrefix` passed by the user when calling a function. If your plugin emits events, prefix the event name with this.
+ */
+
+/**
+ * @typedef {Object} GitHttpResponse
+ * @property {string} url - The final URL that was fetched after any redirects
+ * @property {string} [method] - The HTTP method that was used
+ * @property {Object<string, string>} [headers] - HTTP response headers
+ * @property {AsyncIterableIterator<Uint8Array>} [body] - An async iterator of Uint8Arrays that make up the body of the response
+ * @property {number} statusCode - The HTTP status code
+ * @property {string} statusMessage - The HTTP status message
+ */
+
+/**
+ * HttpClient
+ *
+ * @param {GitHttpRequest} request
+ * @returns {Promise<GitHttpResponse>}
+ */
+export async function request({
+  onProgress,
+  url,
+  method = 'GET',
+  headers = {},
+  body,
+}) {
+  // streaming uploads aren't possible yet in the browser
+  if (body) {
+    body = await collect(body)
+  }
+  const res = await fetch(url, { method, headers, body })
+  const iter =
+    res.body && res.body.getReader
+      ? fromStream(res.body)
+      : [new Uint8Array(await res.arrayBuffer())]
+  // convert Header object to ordinary JSON
+  headers = {}
+  for (const [key, value] of res.headers.entries()) {
+    headers[key] = value
+  }
+  return {
+    url: res.url,
+    method: res.method,
+    statusCode: res.status,
+    statusMessage: res.statusText,
+    body: iter,
+    headers: headers,
+  }
+}
+
+export default { request }
