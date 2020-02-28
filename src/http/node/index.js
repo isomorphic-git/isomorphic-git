1a4a8f82c989b1436eeeff8e47e93cfcca8936d7
William Hilton <wmhilton@gmail.com>
2020-02-28 15:48:31 -0500

    fix: correct TS definition for `http`

--- src/http/node/index.js
+++ src/http/node/index.js
@@ -1,32 +1,9 @@
 import get from 'simple-get'
 
+import '../../typedefs-http.js'
 import { asyncIteratorToStream } from '../../utils/asyncIteratorToStream.js'
 import { collect } from '../../utils/collect.js'
 import { fromNodeStream } from '../../utils/fromNodeStream.js'
-
-// Sorry for the copy & paste from typedefs.js but if we import typedefs.js we'll get a whole bunch of extra comments
-// in the rollup output
-
-/**
- * @typedef {Object} GitHttpRequest
- * @property {string} url - The URL to request
- * @property {string} [method='GET'] - The HTTP method to use
- * @property {Object<string, string>} [headers={}] - Headers to include in the HTTP request
- * @property {AsyncIterableIterator<Uint8Array>} [body] - An async iterator of Uint8Arrays that make up the body of POST requests
- * @property {string} [core] - If your `http` plugin needs access to other plugins, it can do so via `git.cores.get(core)`
- * @property {GitEmitterPlugin} [emitter] - If your `http` plugin emits events, it can do so via `emitter.emit()`
- * @property {string} [emitterPrefix] - The `emitterPrefix` passed by the user when calling a function. If your plugin emits events, prefix the event name with this.
- */
-
-/**
- * @typedef {Object} GitHttpResponse
- * @property {string} url - The final URL that was fetched after any redirects
- * @property {string} [method] - The HTTP method that was used
- * @property {Object<string, string>} [headers] - HTTP response headers
- * @property {AsyncIterableIterator<Uint8Array>} [body] - An async iterator of Uint8Arrays that make up the body of the response
- * @property {number} statusCode - The HTTP status code
- * @property {string} statusMessage - The HTTP status message
- */
 
 /**
  * HttpClient
