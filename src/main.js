b628f3b07a445309be73a30bfe3f8b664a8dd3d5
Peter Hoddie <peter@moddable.tech>
2020-05-01 21:48:33 -0700

    start of fs and http

--- src/main.js
+++ src/main.js
@@ -1,18 +1,32 @@
-import { clone } from 'api/clone'
-import { File, Iterator, System } from 'file'
+import {clone} from 'api/clone'
+import {File, Iterator, System} from 'file'
+import {Request} from "http"
+import SecureSocket from "securesocket";
 
-debugger;
-
-const dir = "/";
+const dir = "/Users/hoddie/isomorphic-git";
 class FileSystem {
-	static async readFile(path, options) {
-		debugger;
+	static async readFile(path, options = {}) {
+		if ("utf8" !== options.encoding)
+			throw new Error("unrecognized encoding");
+
+		const f = new File(path);
+		const result = f.read(String)
+		f.close();
+		return result;
 	}
 	static async writeFile(path, content, options) {
-		debugger;
+		File.delete(path);
+		const f = new File(path, true);
+		f.write(content)
+		f.close();
 	}
 	static async mkdir(path) {
-		debugger;
+		const parts = path.split("/");
+		const dir = [parts.shift()];
+		while (parts.length) {
+			dir.push(parts.shift());
+			File.createDirectory(dir.join("/"));
+		}
 	}
 	static async rmdir() {
 		debugger;
@@ -42,15 +56,37 @@ class FileSystem {
 	}
 }
 
-const http = {
-	"kind": "http"
-};
+class http {
+	static async discover() {
+		debugger;
+	}
+	static async request(options) {
+		const parts = options.url.split("/");
+		if ("https:" !== parts.shift())
+			throw new Error("https only");
+		if (parts.shift())
+			throw new Error("malformed");
+		const host = parts.shift();
+		const path = "/" + parts.join("/");
+
+		return new Promise((resolve, reject) => {
+			const request = new Request({host, path, response: String,
+					port: 443, Socket: SecureSocket, secure: {protocolVersion: 0x303}});
+			request.callback = function(message, value) {
+			//@@ form eexpect reseponse
+				if (Request.responseComplete === message)
+					resolve(value);
+				else if (message < 0)
+					reject(-1);
+			}
+		});
+	}
+}
 
 await clone({
   fs: {promises: FileSystem},
   http,
   dir,
-  corsProxy: 'https://cors.isomorphic-git.org',
   url: 'https://github.com/isomorphic-git/isomorphic-git',
   ref: 'master',
   singleBranch: true,
