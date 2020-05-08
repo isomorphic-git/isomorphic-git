ea09ad6db2a75d05aefb10fd0132b9357e5ceea0
Peter Hoddie <peter@moddable.tech>
2020-05-07 21:02:14 -0700

    static inflate function

--- packages/stub/pako.js
+++ packages/stub/pako.js
@@ -1,7 +1,19 @@
 import Inflate from "mod/inflate"
 
 const pako = Object.freeze({
-	Inflate
+	Inflate,
+	inflate
 });
+
+function inflate(buffer, options = {}) {
+	if (ArrayBuffer.isView(buffer))
+		buffer = buffer.buffer;
+
+	const i = new Inflate(options);
+	i.push(buffer, true);
+	if (i.err)
+		throw new Error;
+	return ("string" === options.to) ? String.fromArrayBuffer(i.result) : i.result;
+}
 
 export default pako;
