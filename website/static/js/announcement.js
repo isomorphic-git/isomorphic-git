959ecd6e8cf008a7817cfd9451e4c15d3e4a43e5
William Hilton <wmhilton@gmail.com>
2020-06-09 20:42:18 -0400

    #BlackLivesMatter

--- website/static/js/announcement.js
+++ website/static/js/announcement.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let announcement = ''
+  let announcement = '#BlackLivesMatter #DefundThePolice'
   if (announcement) {
     let div = document.createElement('div')
-    div.style.background = 'rgba(255,229,100)'
+    div.style.background = 'rgba(0,0,0)' // 'rgba(255,229,100)'
     div.style.textAlign = 'center'
-    div.style.color = 'black'
+    div.style.color = 'white' // 'black'
     div.style.fontSize = '20px'
     div.style.lineHeight = '2em'
     div.style.maxHeight = 'fit-content'
