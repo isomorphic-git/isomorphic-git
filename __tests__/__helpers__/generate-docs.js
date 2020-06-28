ed9083180228948f9286a9b33a6539d45a9b1754
William Hilton <wmhilton@gmail.com>
2020-06-28 00:33:12 -0400

    chore: fix generate-docs.js to use 'main' instead of 'master'

--- __tests__/__helpers__/generate-docs.js
+++ __tests__/__helpers__/generate-docs.js
@@ -291,7 +291,7 @@ console.log('done')
 (function rewriteEditLink() {
   const el = document.querySelector('a.edit-page-link.button');
   if (el) {
-    el.href = 'https://github.com/isomorphic-git/isomorphic-git/edit/master/src/api/${obj.name}.js';
+    el.href = 'https://github.com/isomorphic-git/isomorphic-git/edit/main/src/api/${obj.name}.js';
   }
 })();
 </script>`
