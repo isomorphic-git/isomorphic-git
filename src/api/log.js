4c5a86ffa2ebc272e1adc163e057af42132ab823
Riceball LEE <snowyu.lee@gmail.com>
2021-07-02 18:48:28 +0800

    feat: git log the history of one file only (#1172)
    * feat: log a single file history of commits
    
    * feat: log a single file history of commits with follow supports. resolved #933
    
    * chore(test): jasmine do not support the rejects.toThrowError
    
    * docs: add @snowyu as a contributor
    
    * fix: log file should not add commit when since time
    
    * chore(test): update fixture to check multi deletions log
    
    * chore(test): refresh test snapshot for multi deletions log
    
    * fix(rename): do not search the empty content as filepath in tree
    
    * fix: can not show all history of multi deletions log
    
    * feat: return all the filepaths of the same content in the tree
    
    * fix: git --follow should work with multi same content file
    
    * test: add test git follow with multi same content files
    
    * test(log-file): remove unused example git hooks
    
    * chore(log-file): fix declaraton document for force and follow options
    
    * chore(log-file): correct the declarayion doc
    
    * chore(log-file): correct the declarayion doc in commands

--- src/api/log.js
+++ src/api/log.js
@@ -13,9 +13,12 @@ import { join } from '../utils/join.js'
  * @param {FsClient} args.fs - a file system client
  * @param {string} [args.dir] - The [working tree](dir-vs-gitdir.md) directory path
  * @param {string} [args.gitdir=join(dir,'.git')] - [required] The [git directory](dir-vs-gitdir.md) path
+ * @param {string=} args.filepath optional get the commit for the filepath only
  * @param {string} [args.ref = 'HEAD'] - The commit to begin walking backwards through the history from
- * @param {number} [args.depth] - Limit the number of commits returned. No limit by default.
+ * @param {number=} [args.depth] - Limit the number of commits returned. No limit by default.
  * @param {Date} [args.since] - Return history newer than the given date. Can be combined with `depth` to get whichever is shorter.
+ * @param {boolean=} [args.force=false] do not throw error if filepath is not exist (works only for a single file). defaults to false
+ * @param {boolean=} [args.follow=false] Continue listing the history of a file beyond renames (works only for a single file). defaults to false
  * @param {object} [args.cache] - a [cache](cache.md) object
  *
  * @returns {Promise<Array<ReadCommitResult>>} Resolves to an array of ReadCommitResult objects
@@ -36,9 +39,12 @@ export async function log({
   fs,
   dir,
   gitdir = join(dir, '.git'),
+  filepath,
   ref = 'HEAD',
   depth,
   since, // Date
+  force,
+  follow,
   cache = {},
 }) {
   try {
@@ -50,9 +56,12 @@ export async function log({
       fs: new FileSystem(fs),
       cache,
       gitdir,
+      filepath,
       ref,
       depth,
       since,
+      force,
+      follow,
     })
   } catch (err) {
     err.caller = 'git.log'
