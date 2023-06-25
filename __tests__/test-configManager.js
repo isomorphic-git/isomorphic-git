e82b5747a598e2ec9ae95d091dc50ed3be987645
jayree <jayree.git@icloud.com>
2023-06-25 12:03:26 +0200

    fix: respect git config environment variables

--- __tests__/test-configManager.js
+++ __tests__/test-configManager.js
@@ -1,69 +1,183 @@
 /* eslint-env node, browser, jasmine */
+const path = require('path')
+
 const { GitConfigManager, GitConfig } = require('isomorphic-git/internal-apis')
 
 const { makeFixture } = require('./__helpers__/FixtureFS.js')
 
 describe('config', () => {
-  let fs, gitdir
+  let fs, gitdir, dir
   let oldHome, oldXDGConfigHome
-  let config
 
   beforeAll(async () => {
     // Setup
     const output = await makeFixture('test-config')
     fs = output.fs
     gitdir = output.gitdir
+    dir = output.dir
 
     oldHome = process.env.HOME
     oldXDGConfigHome = process.env.XDG_CONFIG_HOME
-    process.env.HOME = gitdir
-    process.env.XDG_CONFIG_HOME = gitdir
 
-    config = await GitConfigManager.get({
-      fs,
-      gitdir,
-    })
+    process.env.GIT_CONFIG_SYSTEM = path.join(dir, 'git', 'config')
+    process.env.GIT_CONFIG_GLOBAL = path.join(dir, 'gitconfig')
   })
 
   afterAll(async () => {
     process.env.HOME = oldHome
     process.env.XDG_CONFIG_HOME = oldXDGConfigHome
   })
-  ;(process.browser ? xit : it)(
-    'get git XDG and user config values',
-    async () => {
-      const symlinksXDG = await config.get('core.symlinks')
-      const ignorecaseUser = await config.get('core.ignorecase')
-      const urlLocal = await config.get('remote.origin.url')
-      const bareGlobalLocal = await config.get('core.bare')
+  it('get git global config values (symlink)', async () => {
+    const config = await GitConfigManager.get({
+      fs,
+      gitdir,
+    })
 
-      expect(bareGlobalLocal).toBe(false)
-      expect(symlinksXDG).toBe(false)
-      expect(ignorecaseUser).toBe(true)
-      expect(urlLocal).toBe('https://github.com/isomorphic-git/isomorphic-git')
-    }
-  )
-  ;(process.browser ? xit : it)(
-    'save does not set git XDG and user config values',
-    async () => {
-      await GitConfigManager.save({
-        fs,
-        gitdir,
-        config,
+    const symlinksXDGorSystem = await config.get('core.symlinks')
+
+    const systemConfig = GitConfig.from(
+      await fs.read(process.env.GIT_CONFIG_SYSTEM, {
+        encoding: 'utf8',
       })
+    )
 
-      const localConfig = GitConfig.from(
-        await fs.read(`${gitdir}/config`, {
-          encoding: 'utf8',
-        })
-      )
+    expect(process.env.GIT_CONFIG_SYSTEM).toEqual(`${dir}/git/config`)
 
-      const symlinksXDG = await localConfig.get('core.symlinks')
-      const ignorecaseUser = await localConfig.get('core.ignorecase')
-      const urlLocal = await localConfig.get('remote.origin.url')
-      expect(symlinksXDG).toBeUndefined()
-      expect(ignorecaseUser).toBeUndefined()
-      expect(urlLocal).toBe('https://github.com/isomorphic-git/isomorphic-git')
-    }
-  )
+    expect(systemConfig.toString()).toContain('symlinks = false')
+    expect(config.toString()).toContain('symlinks = false')
+
+    expect(symlinksXDGorSystem).toEqual(false)
+  })
+  it('get git global config values (manual - append)', async () => {
+    const ca = GitConfig.from(`[foo]
+    keyaaa = valaaa
+    [bar]
+    keyxyz = valbar`)
+    const a = await ca.get('foo.keyaaa')
+    expect(a).toEqual('valaaa')
+
+    const cb = GitConfig.from(`[foo]
+    keybbb = valbbb
+    [bar]
+    keyxyz = valbar`)
+    const b = await cb.get('foo.keybbb')
+    expect(b).toEqual('valbbb')
+
+    const cc = await ca.appendConfig(cb)
+    const c1 = await cc.get('foo.keyaaa')
+    const c2 = await cc.get('foo.keybbb')
+
+    expect(c1).toEqual('valaaa')
+    expect(c2).toEqual('valbbb')
+  })
+  it('get git global config values (manual - append - env)', async () => {
+    const ca = GitConfig.from(
+      await fs.read(process.env.GIT_CONFIG_SYSTEM, {
+        encoding: 'utf8',
+      })
+    )
+    const a = await ca.get('core.symlinks')
+    expect(a).toEqual(false)
+
+    const cb = GitConfig.from(
+      await fs.read(process.env.GIT_CONFIG_GLOBAL, {
+        encoding: 'utf8',
+      })
+    )
+    const b = await cb.get('core.ignorecase')
+    expect(b).toEqual(true)
+
+    const cc = await ca.appendConfig(cb)
+    const c1 = await cc.get('core.symlinks')
+    const c2 = await cc.get('core.ignorecase')
+
+    expect(c1).toEqual(false)
+    expect(c2).toEqual(true)
+  })
+  it('get git global config values (symlink - single)', async () => {
+    const systemConfig = GitConfig.from(
+      await fs.read(process.env.GIT_CONFIG_SYSTEM, {
+        encoding: 'utf8',
+      })
+    )
+
+    expect(process.env.GIT_CONFIG_SYSTEM).toEqual(`${dir}/git/config`)
+
+    expect(await systemConfig.get('core.symlinks')).toEqual(false)
+  })
+  it('get git global config values (ignore)', async () => {
+    const config = await GitConfigManager.get({
+      fs,
+      gitdir,
+    })
+
+    const ignorecaseUser = await config.get('core.ignorecase')
+
+    const globalConfig = GitConfig.from(
+      await fs.read(process.env.GIT_CONFIG_GLOBAL, {
+        encoding: 'utf8',
+      })
+    )
+
+    expect(process.env.GIT_CONFIG_GLOBAL).toEqual(`${dir}/gitconfig`)
+
+    expect(globalConfig.toString()).toContain('ignorecase = true')
+    expect(config.toString()).toContain('ignorecase = true')
+
+    expect(ignorecaseUser).toEqual(true)
+  })
+  it('get git global config values (ignore - single)', async () => {
+    const systemConfig = GitConfig.from(
+      await fs.read(process.env.GIT_CONFIG_GLOBAL, {
+        encoding: 'utf8',
+      })
+    )
+
+    expect(process.env.GIT_CONFIG_GLOBAL).toEqual(`${dir}/gitconfig`)
+
+    expect(await systemConfig.get('core.ignorecase')).toEqual(true)
+  })
+  it('get git global config values (url)', async () => {
+    const config = await GitConfigManager.get({
+      fs,
+      gitdir,
+    })
+
+    const urlLocal = await config.get('remote.origin.url')
+    expect(urlLocal).toEqual('https://github.com/isomorphic-git/isomorphic-git')
+  })
+  it('get git global config values (bare)', async () => {
+    const config = await GitConfigManager.get({
+      fs,
+      gitdir,
+    })
+
+    const bareGlobalLocal = await config.get('core.bare')
+    expect(bareGlobalLocal).toEqual(false)
+  })
+  it('save does not set global config values (local)', async () => {
+    const config = await GitConfigManager.get({
+      fs,
+      gitdir,
+    })
+
+    await GitConfigManager.save({
+      fs,
+      gitdir,
+      config,
+    })
+
+    const localConfig = GitConfig.from(
+      await fs.read(`${gitdir}/config`, {
+        encoding: 'utf8',
+      })
+    )
+
+    const symlinksXDG = await localConfig.get('core.symlinks')
+    const ignorecaseUser = await localConfig.get('core.ignorecase')
+    const urlLocal = await localConfig.get('remote.origin.url')
+    expect(symlinksXDG).toBeUndefined()
+    expect(ignorecaseUser).toBeUndefined()
+    expect(urlLocal).toBe('https://github.com/isomorphic-git/isomorphic-git')
+  })
 })
--- src/managers/GitConfigManager.js
+++ src/managers/GitConfigManager.js
@@ -2,26 +2,18 @@ import path from 'path'
 
 import { GitConfig } from '../models/GitConfig.js'
 
-async function readConfig(fs, path, type) {
-  let config = new GitConfig()
-  try {
-    config = GitConfig.from(
-      await fs.read(path, {
-        encoding: 'utf8',
-      }),
-      type
-    )
-  } catch (err) {}
-  return config
-}
-
-export class GitConfigManager {
-  static async get({ fs, gitdir }) {
-    let globalConfig = new GitConfig()
-    if (!process.browser) {
+function getSystemConfigPath() {
+  let systemConfigPath
+  if (!(process.env.GIT_CONFIG_NOSYSTEM === '1')) {
+    if (process.env.GIT_CONFIG_SYSTEM) {
+      systemConfigPath = process.env.GIT_CONFIG_SYSTEM
+    } else {
       const platform = process.platform
 
-      let systemConfigPath = ''
+      if (!platform) {
+        throw new Error('no platform')
+      }
+
       if (platform === 'win32') {
         systemConfigPath = path.join(
           'C:',
@@ -44,24 +36,58 @@ export class GitConfigManager {
           'gitconfig'
         )
       }
-      const systemConfig = await readConfig(fs, systemConfigPath, 'system')
-
-      const HOME = process.env.HOME || ''
-      const XDG_CONFIG_HOME =
-        process.env.XDG_CONFIG_HOME || (HOME && path.join(HOME, '.config'))
-
-      const globalXDGConfig = await (
-        await readConfig(
-          fs,
-          path.join(XDG_CONFIG_HOME, 'git', 'config'),
-          'global'
-        )
-      ).appendConfig(systemConfig)
-
-      globalConfig = await (
-        await readConfig(fs, path.join(HOME, '.gitconfig'), 'global')
-      ).appendConfig(globalXDGConfig)
     }
+  }
+
+  return systemConfigPath
+}
+
+function getGlobalConfigPath() {
+  return process.env.HOME && path.join(process.env.HOME, '.gitconfig')
+}
+
+function getGlobalXDGConfigPath() {
+  return (
+    (process.env.XDG_CONFIG_HOME || process.env.HOME) &&
+    path.join(
+      process.env.XDG_CONFIG_HOME || path.join(process.env.HOME, '.config'),
+      'git',
+      'config'
+    )
+  )
+}
+
+async function readConfig(fs, path, type) {
+  let config = new GitConfig()
+  // try {
+  config = GitConfig.from(
+    await fs.read(path, {
+      encoding: 'utf8',
+    }),
+    type
+  )
+  // } catch (err) {}
+  return config
+}
+
+export class GitConfigManager {
+  static async get({ fs, gitdir }) {
+    const systemConfig = await readConfig(fs, getSystemConfigPath(), 'system')
+
+    let globalConfigPath = ''
+    let globalXDGConfig = new GitConfig()
+    if (process.env.GIT_CONFIG_GLOBAL) {
+      globalXDGConfig = systemConfig
+      globalConfigPath = process.env.GIT_CONFIG_GLOBAL
+    } else {
+      globalXDGConfig = await (
+        await readConfig(fs, getGlobalXDGConfigPath(), 'global')
+      ).appendConfig(systemConfig)
+      globalConfigPath = getGlobalConfigPath()
+    }
+    const globalConfig = await (
+      await readConfig(fs, globalConfigPath, 'global')
+    ).appendConfig(globalXDGConfig)
 
     const localConfig = await (
       await readConfig(fs, path.join(gitdir, 'config'))
