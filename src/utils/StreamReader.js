2fb3bc4ec44e085bc1811d2f7b20cb339a35faa6
Dan Allen <dan.j.allen@gmail.com>
2023-12-07 02:20:15 -0700

    add null check before comparing buffer length to cursor when looking for end of stream

--- src/utils/StreamReader.js
+++ src/utils/StreamReader.js
@@ -13,7 +13,9 @@ export class StreamReader {
   }
 
   eof() {
-    return this._ended && this.cursor === this.buffer.length
+    return (
+      this._ended && this.buffer != null && this.cursor === this.buffer.length
+    )
   }
 
   tell() {
