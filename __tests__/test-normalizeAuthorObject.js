fa654dbb6c6f531e4ebfc21317d6247e9d110c2b
hhourani27 <h.hourany@gmail.com>
2024-07-05 11:09:57 +0200

    add commit param to normalizeAuthorObject

--- __tests__/test-normalizeAuthorObject.js
+++ __tests__/test-normalizeAuthorObject.js
@@ -35,7 +35,49 @@ describe('normalizeAuthorObject', () => {
     expect(await normalizeAuthorObject({ fs, gitdir, author })).toEqual(author)
   })
 
-  it('return config values and new timestamp if no author was provided', async () => {
+  it('return commit author when no author was provided', async () => {
+    // Setup
+    const { fs, gitdir } = await makeFixture('test-normalizeAuthorObject')
+
+    await setConfig({
+      fs,
+      gitdir,
+      path: 'user.name',
+      value: `user-config`,
+    })
+
+    await setConfig({
+      fs,
+      gitdir,
+      path: 'user.email',
+      value: `user-config@example.com`,
+    })
+
+    // Test
+    const commit = {
+      message: 'commit message',
+      tree: '80655da8d80aaaf92ce5357e7828dc09adb00993', // Just random SHA-1
+      parent: ['d8fd39d0bbdd2dcf322d8b11390a4c5825b11495'], // Just random SHA-1
+      author: {
+        name: 'commit-author',
+        email: 'commit-author@example.com',
+        timestamp: 1720169744,
+        timezoneOffset: 60,
+      },
+      committer: {
+        name: 'commit-commiter',
+        email: 'commit-commiter@example.com',
+        timestamp: 1720169744,
+        timezoneOffset: 120,
+      },
+    }
+
+    expect(await normalizeAuthorObject({ fs, gitdir, commit })).toEqual(
+      commit.author
+    )
+  })
+
+  it('return config values and new timestamp if no author or commit was provided', async () => {
     // Setup
     const { fs, gitdir } = await makeFixture('test-normalizeAuthorObject')
 
--- src/utils/normalizeAuthorObject.js
+++ src/utils/normalizeAuthorObject.js
@@ -1,29 +1,52 @@
+import '../typedefs.js'
+
 import { _getConfig } from '../commands/getConfig'
 
 /**
  * Returns an author object by populating properties from the provided author object,
- * and falling back to Config and current date/time for missing properties.
+ * and falling back first to the author of commit object, then to the Config and current date/time for missing properties.
  *
  * @param {Object} args
  * @param {FsClient} args.fs - a file system implementation
  * @param {string} [args.gitdir] - The [git directory](dir-vs-gitdir.md) path
  * @param {Object} [args.author={}] - The author object.
+ * @param {CommitObject} [args.commit={}] - A commit object.
  *
  * @returns {Promise<void | {name: string, email: string, timestamp: number, timezoneOffset: number }>}
  */
-export async function normalizeAuthorObject({ fs, gitdir, author = {} }) {
-  let { name, email, timestamp, timezoneOffset } = author
-  name = name || (await _getConfig({ fs, gitdir, path: 'user.name' }))
-  email = email || (await _getConfig({ fs, gitdir, path: 'user.email' })) || ''
+export async function normalizeAuthorObject({
+  fs,
+  gitdir,
+  author = {},
+  commit = {},
+}) {
+  const commitAuthor = commit.author ? commit.author : {}
+
+  const name =
+    author.name ||
+    commitAuthor.name ||
+    (await _getConfig({ fs, gitdir, path: 'user.name' }))
+  const email =
+    author.email ||
+    commitAuthor.email ||
+    (await _getConfig({ fs, gitdir, path: 'user.email' })) ||
+    ''
 
   if (name === undefined) {
     return undefined
   }
 
-  timestamp = timestamp != null ? timestamp : Math.floor(Date.now() / 1000)
-  timezoneOffset =
-    timezoneOffset != null
-      ? timezoneOffset
+  const timestamp =
+    author.timestamp != null
+      ? author.timestamp
+      : commitAuthor.timestamp != null
+      ? commitAuthor.timestamp
+      : Math.floor(Date.now() / 1000)
+  const timezoneOffset =
+    author.timezoneOffset != null
+      ? author.timezoneOffset
+      : commitAuthor.timezoneOffset != null
+      ? commitAuthor.timezoneOffset
       : new Date(timestamp * 1000).getTimezoneOffset()
 
   return { name, email, timestamp, timezoneOffset }
