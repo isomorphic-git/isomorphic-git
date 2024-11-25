37bdb15aa7591be08572c37d2dea98e36247b38b
Sebastien <colladonsebastien@gmail.com>
2024-11-26 00:00:52 +0100

    fix: refactor: use path.join from stdlib instead of custom join (#1958)
    * test: parameterized internal join
    
    * refactor: use `path.normalize` implementation
    
    * fix: import path from internal ACL
    
    * refactor: use path methods
    
    * refactor: use node path
    
    * fix: os compatibility issue with join
    
    * refactor: use `path-browserify` posix implementation

--- src/utils/join.js
+++ src/utils/join.js
@@ -1,7 +1,1 @@
-// For some reason path.posix.join is undefined in webpack
-// Also, this is just much smaller
-import { normalizePath } from './normalizePath.js'
-
-export function join(...parts) {
-  return normalizePath(parts.map(normalizePath).join('/'))
-}
+export { join } from 'path-browserify'
