6add40de1dc306cbfba9c75dca883cc49fb88407
eliot-akira <me@eliotakira.com>
2025-02-06 16:40:41 +0100

    fix: Use default import and destructure method because path-browserify uses module.exports

--- src/utils/join.js
+++ src/utils/join.js
@@ -1,1 +1,6 @@
-export { join } from 'path-browserify'
+// Use default import and destructure method because path-browserify uses module.exports
+import path from 'path-browserify'
+
+const { join } = path
+
+export { join }
