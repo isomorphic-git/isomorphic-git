7541c61efaa9cc07a01b2e36535c542dfaacb8ce
eliot-akira <me@eliotakira.com>
2025-04-24 05:22:01 +0200

    Add --experimental-vm-modules flag required by Jest to support ES Module
    - https://github.com/jestjs/jest/issues/9430
    - https://github.com/isomorphic-git/isomorphic-git/pull/2035#issuecomment-2688414611

--- package-scripts.cjs
+++ package-scripts.cjs
@@ -134,8 +134,8 @@ module.exports = {
       setup: series.nps('proxy.start', 'gitserver.start'),
       teardown: series.nps('proxy.stop', 'gitserver.stop'),
       jest: process.env.CI
-        ? retry3(`${timeout5('jest --ci --coverage')}`)
-        : `jest --ci --coverage`,
+        ? retry3(`${timeout5('node --experimental-vm-modules node_modules/jest/bin/jest.js --ci --coverage')}`)
+        : `node --experimental-vm-modules node_modules/jest/bin/jest.js --ci --coverage`,
       karma: process.env.CI
         ? retry3('karma start ./karma.conf.cjs --single-run')
         : 'cross-env karma start ./karma.conf.cjs --single-run -log-level debug',
