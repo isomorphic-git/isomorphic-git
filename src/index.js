3ee08f795bce4167c4f5039c7483211b10b9fd61
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-08-20 17:47:49 +0200

    Update index.js
    reduced type quirks and also module import require quirks now all get the same object.

--- src/index.js
+++ src/index.js
@@ -1,220 +1,72 @@
 import './typedefs.js'
 
-import { STAGE } from './api/STAGE.js'
-import { TREE } from './api/TREE.js'
-import { WORKDIR } from './api/WORKDIR.js'
-import { abortMerge } from './api/abortMerge.js'
-import { add } from './api/add.js'
-import { addNote } from './api/addNote.js'
-import { addRemote } from './api/addRemote.js'
-import { annotatedTag } from './api/annotatedTag.js'
-import { branch } from './api/branch.js'
-import { checkout } from './api/checkout.js'
-import { clone } from './api/clone.js'
-import { commit } from './api/commit.js'
-import { currentBranch } from './api/currentBranch.js'
-import { deleteBranch } from './api/deleteBranch.js'
-import { deleteRef } from './api/deleteRef.js'
-import { deleteRemote } from './api/deleteRemote.js'
-import { deleteTag } from './api/deleteTag.js'
-import { expandOid } from './api/expandOid.js'
-import { expandRef } from './api/expandRef.js'
-import { fastForward } from './api/fastForward.js'
-import { fetch } from './api/fetch.js'
-import { findMergeBase } from './api/findMergeBase.js'
-import { findRoot } from './api/findRoot.js'
-import { getConfig } from './api/getConfig.js'
-import { getConfigAll } from './api/getConfigAll.js'
-import { getRemoteInfo } from './api/getRemoteInfo.js'
-import { getRemoteInfo2 } from './api/getRemoteInfo2.js'
-import { hashBlob } from './api/hashBlob.js'
-import { indexPack } from './api/indexPack.js'
-import { init } from './api/init.js'
-import { isDescendent } from './api/isDescendent.js'
-import { isIgnored } from './api/isIgnored.js'
-import { listBranches } from './api/listBranches.js'
-import { listFiles } from './api/listFiles.js'
-import { listNotes } from './api/listNotes.js'
-import { listRefs } from './api/listRefs.js'
-import { listRemotes } from './api/listRemotes.js'
-import { listServerRefs } from './api/listServerRefs.js'
-import { listTags } from './api/listTags.js'
-import { log } from './api/log.js'
-import { merge } from './api/merge.js'
-import { packObjects } from './api/packObjects.js'
-import { pull } from './api/pull.js'
-import { push } from './api/push.js'
-import { readBlob } from './api/readBlob.js'
-import { readCommit } from './api/readCommit.js'
-import { readNote } from './api/readNote.js'
-import { readObject } from './api/readObject.js'
-import { readTag } from './api/readTag.js'
-import { readTree } from './api/readTree.js'
-import { remove } from './api/remove.js'
-import { removeNote } from './api/removeNote.js'
-import { renameBranch } from './api/renameBranch.js'
-import { resetIndex } from './api/resetIndex.js'
-import { resolveRef } from './api/resolveRef.js'
-import { setConfig } from './api/setConfig.js'
-import { stash } from './api/stash.js'
-import { status } from './api/status.js'
-import { statusMatrix } from './api/statusMatrix.js'
-import { tag } from './api/tag.js'
-import { updateIndex } from './api/updateIndex.js'
-import { version } from './api/version.js'
-import { walk } from './api/walk.js'
-import { writeBlob } from './api/writeBlob.js'
-import { writeCommit } from './api/writeCommit.js'
-import { writeObject } from './api/writeObject.js'
-import { writeRef } from './api/writeRef.js'
-import { writeTag } from './api/writeTag.js'
-import { writeTree } from './api/writeTree.js'
-import * as Errors from './errors/index.js'
-
-// named exports
-export {
-  Errors,
-  STAGE,
-  TREE,
-  WORKDIR,
-  abortMerge,
-  add,
-  addNote,
-  addRemote,
-  annotatedTag,
-  branch,
-  checkout,
-  clone,
-  commit,
-  getConfig,
-  getConfigAll,
-  setConfig,
-  currentBranch,
-  deleteBranch,
-  deleteRef,
-  deleteRemote,
-  deleteTag,
-  expandOid,
-  expandRef,
-  fastForward,
-  fetch,
-  findMergeBase,
-  findRoot,
-  getRemoteInfo,
-  getRemoteInfo2,
-  hashBlob,
-  indexPack,
-  init,
-  isDescendent,
-  isIgnored,
-  listBranches,
-  listFiles,
-  listNotes,
-  listRefs,
-  listRemotes,
-  listServerRefs,
-  listTags,
-  log,
-  merge,
-  packObjects,
-  pull,
-  push,
-  readBlob,
-  readCommit,
-  readNote,
-  readObject,
-  readTag,
-  readTree,
-  remove,
-  removeNote,
-  renameBranch,
-  resetIndex,
-  updateIndex,
-  resolveRef,
-  status,
-  statusMatrix,
-  tag,
-  version,
-  walk,
-  writeBlob,
-  writeCommit,
-  writeObject,
-  writeRef,
-  writeTag,
-  writeTree,
-  stash,
-}
-
-// default export
-export default {
-  Errors,
-  STAGE,
-  TREE,
-  WORKDIR,
-  add,
-  abortMerge,
-  addNote,
-  addRemote,
-  annotatedTag,
-  branch,
-  checkout,
-  clone,
-  commit,
-  getConfig,
-  getConfigAll,
-  setConfig,
-  currentBranch,
-  deleteBranch,
-  deleteRef,
-  deleteRemote,
-  deleteTag,
-  expandOid,
-  expandRef,
-  fastForward,
-  fetch,
-  findMergeBase,
-  findRoot,
-  getRemoteInfo,
-  getRemoteInfo2,
-  hashBlob,
-  indexPack,
-  init,
-  isDescendent,
-  isIgnored,
-  listBranches,
-  listFiles,
-  listNotes,
-  listRefs,
-  listRemotes,
-  listServerRefs,
-  listTags,
-  log,
-  merge,
-  packObjects,
-  pull,
-  push,
-  readBlob,
-  readCommit,
-  readNote,
-  readObject,
-  readTag,
-  readTree,
-  remove,
-  removeNote,
-  renameBranch,
-  resetIndex,
-  updateIndex,
-  resolveRef,
-  status,
-  statusMatrix,
-  tag,
-  version,
-  walk,
-  writeBlob,
-  writeCommit,
-  writeObject,
-  writeRef,
-  writeTag,
-  writeTree,
-  stash,
-}
+export { STAGE } from './api/STAGE.js';
+export { TREE } from './api/TREE.js';
+export { WORKDIR } from './api/WORKDIR.js';
+export { abortMerge } from './api/abortMerge.js';
+export { add } from './api/add.js';
+export { addNote } from './api/addNote.js';
+export { addRemote } from './api/addRemote.js';
+export { annotatedTag } from './api/annotatedTag.js';
+export { branch } from './api/branch.js';
+export { checkout } from './api/checkout.js';
+export { clone } from './api/clone.js';
+export { commit } from './api/commit.js';
+export { currentBranch } from './api/currentBranch.js';
+export { deleteBranch } from './api/deleteBranch.js';
+export { deleteRef } from './api/deleteRef.js';
+export { deleteRemote } from './api/deleteRemote.js';
+export { deleteTag } from './api/deleteTag.js';
+export { expandOid } from './api/expandOid.js';
+export { expandRef } from './api/expandRef.js';
+export { fastForward } from './api/fastForward.js';
+export { fetch } from './api/fetch.js';
+export { findMergeBase } from './api/findMergeBase.js';
+export { findRoot } from './api/findRoot.js';
+export { getConfig } from './api/getConfig.js';
+export { getConfigAll } from './api/getConfigAll.js';
+export { getRemoteInfo } from './api/getRemoteInfo.js';
+export { getRemoteInfo2 } from './api/getRemoteInfo2.js';
+export { hashBlob } from './api/hashBlob.js';
+export { indexPack } from './api/indexPack.js';
+export { init } from './api/init.js';
+export { isDescendent } from './api/isDescendent.js';
+export { isIgnored } from './api/isIgnored.js';
+export { listBranches } from './api/listBranches.js';
+export { listFiles } from './api/listFiles.js';
+export { listNotes } from './api/listNotes.js';
+export { listRefs } from './api/listRefs.js';
+export { listRemotes } from './api/listRemotes.js';
+export { listServerRefs } from './api/listServerRefs.js';
+export { listTags } from './api/listTags.js';
+export { log } from './api/log.js';
+export { merge } from './api/merge.js';
+export { packObjects } from './api/packObjects.js';
+export { pull } from './api/pull.js';
+export { push } from './api/push.js';
+export { readBlob } from './api/readBlob.js';
+export { readCommit } from './api/readCommit.js';
+export { readNote } from './api/readNote.js';
+export { readObject } from './api/readObject.js';
+export { readTag } from './api/readTag.js';
+export { readTree } from './api/readTree.js';
+export { remove } from './api/remove.js';
+export { removeNote } from './api/removeNote.js';
+export { renameBranch } from './api/renameBranch.js';
+export { resetIndex } from './api/resetIndex.js';
+export { resolveRef } from './api/resolveRef.js';
+export { setConfig } from './api/setConfig.js';
+export { stash } from './api/stash.js';
+export { status } from './api/status.js';
+export { statusMatrix } from './api/statusMatrix.js';
+export { tag } from './api/tag.js';
+export { updateIndex } from './api/updateIndex.js';
+export { version } from './api/version.js';
+export { walk } from './api/walk.js';
+export { writeBlob } from './api/writeBlob.js';
+export { writeCommit } from './api/writeCommit.js';
+export { writeObject } from './api/writeObject.js';
+export { writeRef } from './api/writeRef.js';
+export { writeTag } from './api/writeTag.js';
+export { writeTree } from './api/writeTree.js';
+export * as Errors from './errors/index.js';
