48fad05b6115a3ea54b9f58c4ddeecb2222fd742
James Prevett <jp@jamespre.dev>
2025-09-14 22:09:48 -0500

    Inline some utility functions

--- __tests__/__helpers__/FixtureFS/makeNodeFixture.js
+++ __tests__/__helpers__/FixtureFS/makeNodeFixture.js
@@ -1,16 +1,53 @@
+import * as fs from 'fs'
+import * as os from 'os'
 import * as path from 'path'
 import { fileURLToPath } from 'url'
 
+import findUp from 'find-up'
 import { FileSystem } from 'isomorphic-git/internal-apis'
+import onExit from 'signal-exit'
+
+const TEMP_PATH = path.join(os.tmpdir(), 'jest-fixture-')
+const TEMP_DIRS_CREATED = []
+
+/**
+ * @param {string} cwd
+ * @param  {...string} fileParts
+ */
+export function getFixturePath(cwd, ...fileParts) {
+  return findUp(path.join('__fixtures__', ...fileParts), { cwd })
+}
+
+export async function createTempDir() {
+  const tempDir = await fs.promises.mkdtemp(TEMP_PATH)
+  TEMP_DIRS_CREATED.push(tempDir)
+  return tempDir
+}
+
+export function cleanupTempDirs() {
+  for (const tempDir of TEMP_DIRS_CREATED) {
+    try {
+      fs.rmSync(tempDir, { recursive: true, force: true })
+    } catch (err) {}
+  }
+
+  TEMP_DIRS_CREATED.length = 0
+}
+
+/**
+ * @param {string} cwd
+ * @param  {...string} fileParts
+ */
+export async function copyFixtureIntoTempDir(cwd, ...fileParts) {
+  const fixturePath = await getFixturePath(cwd, ...fileParts)
+  const tempDir = await createTempDir()
+  await fs.promises.cp(fixturePath, tempDir, { recursive: true })
+  return tempDir
+}
 
 export async function makeNodeFixture(fixture) {
-  const {
-    getFixturePath,
-    createTempDir,
-    copyFixtureIntoTempDir,
-  } = await import('./_jestUtils.js')
-
   const _fs = await import('fs')
+  onExit(cleanupTempDirs)
 
   const fs = new FileSystem(_fs)
 
