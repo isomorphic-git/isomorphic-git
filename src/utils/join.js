09b8f9e2a6ee1159f69448418f5bdfc61a841b30
James Prevett <jp@jamespre.dev>
2025-09-14 21:20:59 -0500

    Fixed join

--- src/utils/join.js
+++ src/utils/join.js
@@ -1,1 +1,25 @@
-export { join } from '@zenfs/core/path'
+import { normalize } from '@zenfs/core/path'
+
+function assertPath(path) {
+  if (typeof path !== 'string') {
+    throw new TypeError(
+      'Path must be a string. Received ' + JSON.stringify(path)
+    )
+  }
+}
+
+// ZenFS does not handle "" parts the same (yet)
+export function join(...args) {
+  if (args.length === 0) return '.'
+  let joined
+  for (let i = 0; i < args.length; ++i) {
+    const arg = args[i]
+    assertPath(arg)
+    if (arg.length > 0) {
+      if (joined === undefined) joined = arg
+      else joined += '/' + arg
+    }
+  }
+  if (joined === undefined) return '.'
+  return normalize(joined)
+}
