d843dd37e51e34b47c591b630629db4d55394c49
James Prevett <jp@jamespre.dev>
2025-09-17 09:49:01 -0500

    Fix karma config to use CJS

--- __tests__/__helpers__/karma-load-successful-browsers.cjs
+++ __tests__/__helpers__/karma-load-successful-browsers.cjs
@@ -1,38 +1,40 @@
-import * as fs from 'fs'
-import * as path from 'path'
+const fs = require('fs')
+const path = require('path')
 
 const translateBrowser = require('./karma-translate-browser.cjs')
 
-export function load() {
-  // Append to the existing list of successful browsers
-  try {
-    const browsers = JSON.parse(
-      fs.readFileSync(path.join(process.cwd(), 'browser-tests.json'), 'utf8')
-    )
-    return [browsers, browsers.map(translateBrowser)]
-  } catch (err) {
-    // file wasn't there, so don't worry
-    return [[], []]
-  }
-}
-export function filter(browsers) {
-  const [, successfulBrowsers] = module.exports.load()
-  console.log('skipping browsers:', successfulBrowsers)
-  const newbrowsers = browsers.filter(b => !successfulBrowsers.includes(b))
+module.exports = {
+  load() {
+    // Append to the existing list of successful browsers
+    try {
+      const browsers = JSON.parse(
+        fs.readFileSync(path.join(process.cwd(), 'browser-tests.json'), 'utf8')
+      )
+      return [browsers, browsers.map(translateBrowser)]
+    } catch (err) {
+      // file wasn't there, so don't worry
+      return [[], []]
+    }
+  },
+  filter(browsers) {
+    const [, successfulBrowsers] = module.exports.load()
+    console.log('skipping browsers:', successfulBrowsers)
+    const newbrowsers = browsers.filter(b => !successfulBrowsers.includes(b))
 
-  if (newbrowsers.length === 0) {
-    console.log(
-      'All browsers already passed test suite. Deleting ./browser-tests.json'
+    if (newbrowsers.length === 0) {
+      console.log(
+        'All browsers already passed test suite. Deleting ./browser-tests.json'
+      )
+      fs.unlinkSync(path.join(process.cwd(), 'browser-tests.json'))
+      process.exit(0)
+    }
+    return newbrowsers
+  },
+  save(successfulBrowsersFullNames) {
+    fs.writeFileSync(
+      path.join(process.cwd(), 'browser-tests.json'),
+      JSON.stringify(successfulBrowsersFullNames, null, 2),
+      'utf8'
     )
-    fs.unlinkSync(path.join(process.cwd(), 'browser-tests.json'))
-    process.exit(0)
-  }
-  return newbrowsers
-}
-export function save(successfulBrowsersFullNames) {
-  fs.writeFileSync(
-    path.join(process.cwd(), 'browser-tests.json'),
-    JSON.stringify(successfulBrowsersFullNames, null, 2),
-    'utf8'
-  )
+  },
 }
