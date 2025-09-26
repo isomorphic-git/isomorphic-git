60807d495a88f4636c474df602cbebdef7e00e23
James Prevett <jp@jamespre.dev>
2025-09-26 16:55:54 -0500

    Remove dead code

--- __tests__/index.webpack.js
+++
@@ -1,9 +0,0 @@
-import { readdirSync } from 'fs'
-// polyfill toMatchInlineSnapshot
-import './__helpers__/jasmine-inline-snapshots.js'
-
-// This used to use webpack's require.context() but that doesn't work with ESM
-// The dynamic import is equivalent though
-for (const path of readdirSync(import.meta.dirname)) {
-  if (path.startsWith('test-')) await import('./' + path)
-}
