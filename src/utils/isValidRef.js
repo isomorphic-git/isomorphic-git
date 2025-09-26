be594306f11a7dc1256d3ac4653acc6172843350
James Prevett <jp@jamespre.dev>
2025-09-26 16:32:19 -0500

    Fix eslint

--- src/utils/isValidRef.js
+++ src/utils/isValidRef.js
@@ -8,7 +8,7 @@ Copyright © Vincent Weevers
 const bad = /(^|[/.])([/.]|$)|^@$|@{|[\x00-\x20\x7f~^:?*[\\]|\.lock(\/|$)/
 
 export default function isValidRef(name, onelevel) {
-  if (typeof name != 'string')
+  if (typeof name !== 'string')
     throw new TypeError('Reference name must be a string')
 
   return !bad.test(name) && (!!onelevel || name.includes('/'))
