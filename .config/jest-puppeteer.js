b3391bc7d29df9b2bc21d43768a71411d570d514
James Prevett <jp@jamespre.dev>
2025-09-26 16:01:30 -0500

    Added headless to browser

--- .config/jest-puppeteer.js
+++ .config/jest-puppeteer.js
@@ -2,5 +2,6 @@ export default {
   launch: {
     browser: process.env.JEST_BROWSER,
     args: ['--no-sandbox'],
+	headless: true,
   },
 }
--- src/utils/isValidRef.js
+++ src/utils/isValidRef.js
@@ -1,5 +1,5 @@
 /*
-Adopted from is-git-ref-name-valid
+Adapted from is-git-ref-name-valid
 SPDX-License-Identifier: MIT
 Copyright © Vincent Weevers
 */
@@ -7,10 +7,8 @@ Copyright © Vincent Weevers
 // eslint-disable-next-line no-control-regex
 const bad = /(^|[/.])([/.]|$)|^@$|@{|[\x00-\x20\x7f~^:?*[\\]|\.lock(\/|$)/
 
-export default function validRef(name, onelevel) {
-  if (typeof name !== 'string') {
-    throw new TypeError('Reference name must be a string')
-  }
+export default function isValidRef(name, onelevel) {
+  if (typeof name != 'string') throw new TypeError('Reference name must be a string')
 
   return !bad.test(name) && (!!onelevel || name.includes('/'))
 }
