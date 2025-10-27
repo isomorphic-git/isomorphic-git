b315993c57936a7b53abc61c7265464bdc02dcd3
Abhinav Singh Pundir <abhinavpundir182005@gmail.com>
2025-10-27 20:24:16 +0530

    fixed stylee

--- src/errors/InternalError.js
+++ src/errors/InternalError.js
@@ -1,4 +1,4 @@
-import { BaseError } from "./BaseError.js";
+import { BaseError } from './BaseError.js'
 
 export class InternalError extends BaseError {
   /**
@@ -7,10 +7,10 @@ export class InternalError extends BaseError {
   constructor(message) {
     super(
       `If you're not a developer, report the bug to the developers of the application you're using. If this is a bug in isomorphic-git they should create a proper bug themselves. The bug should include a minimal reproduction and details about the version and environment.\n\nPlease file a bug report at https://github.com/isomorphic-git/isomorphic-git/issues with this error message: ${message}`
-    );
-    this.code = this.name = InternalError.code;
-    this.data = { message };
+    )
+    this.code = this.name = InternalError.code
+    this.data = { message }
   }
 }
 /** @type {'InternalError'} */
-InternalError.code = "InternalError";
+InternalError.code = 'InternalError'
