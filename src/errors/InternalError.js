a7aa037d61302b51b6544a525d80858c71ce763e
Abhinav Singh Pundir <abhinavpundir182005@gmail.com>
2025-10-28 17:40:44 +0530

    improved version

--- src/errors/InternalError.js
+++ src/errors/InternalError.js
@@ -6,7 +6,7 @@ export class InternalError extends BaseError {
    */
   constructor(message) {
     super(
-      `Please file a bug report at https://github.com/isomorphic-git/isomorphic-git/issues with this error message: ${message}.\n\nIf you're not a developer, report the bug to the developers of the application you're using. If this is a bug in isomorphic-git then you should create a proper bug yourselves. The bug should include a minimal reproduction and details about the version and environment.`
+      `An internal error caused this command to fail.\n\nIf you're not a developer, report the bug to the developers of the application you're using. If this is a bug in isomorphic-git then you should create a proper bug yourselves. The bug should include a minimal reproduction and details about the version and environment.\n\nPlease file a bug report at https://github.com/isomorphic-git/isomorphic-git/issues with this error message: ${message}`
     )
     this.code = this.name = InternalError.code
     this.data = { message }
