5c9c448574be931b2efa2b34f01d02fd533e707a
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-01-21 12:46:01 +0100

    fix tests

--- __tests__/test-walk-in-submodule.js
+++ __tests__/test-walk-in-submodule.js
@@ -264,8 +264,8 @@ describe('walk', () => {
         {
           type: 'blob',
           mode: SYMLINKMODE,
-          content: '',
-          oid: 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
+          content: '1.txt',
+          oid: '7999426c516ffbbae9136d93dc44e89091d35a13',
           hasStat: true,
         },
         null,
@@ -496,8 +496,8 @@ describe('walk', () => {
         {
           type: 'blob',
           mode: SYMLINKMODE,
-          content: '',
-          oid: 'e69de29bb2d1d6434b8b29ae775ad8c2e48c5391',
+          content: '1.txt',
+          oid: '7999426c516ffbbae9136d93dc44e89091d35a13',
           hasStat: true,
         },
         null,
