872f66f089dd6afda1b802d78908e5d4f72181ba
liamhess <me@liamhess.de>
2026-03-12 10:10:18 +0100

    do not import from src and make test minimal to only catch type error

--- __tests__/test-BaseError.js
+++ __tests__/test-BaseError.js
@@ -1,30 +1,13 @@
 /* eslint-env node, browser, jasmine */
 
-// Importing directly from src/ so that tsc --checkJs catches type errors
-// in BaseError.js (e.g. undeclared properties used in toJSON/fromJSON).
-import { BaseError } from '../src/errors/BaseError.js'
+import { Errors } from 'isomorphic-git'
 
 describe('BaseError', () => {
-  it('toJSON includes code and data', () => {
-    const e = new BaseError('test')
-    e.code = 'TestError'
-    e.data = { foo: 1 }
-    const json = e.toJSON()
-    expect(json.code).toBe('TestError')
-    expect(json.data).toEqual({ foo: 1 })
-    expect(json.caller).toBe('')
-    expect(json.message).toBe('test')
-  })
-
-  it('fromJSON round-trips', () => {
-    const original = new BaseError('test')
-    original.code = 'TestError'
-    original.data = { foo: 1 }
-    original.caller = 'myCaller'
-    const restored = original.fromJSON(original.toJSON())
-    expect(restored.code).toBe('TestError')
-    expect(restored.data).toEqual({ foo: 1 })
-    expect(restored.caller).toBe('myCaller')
-    expect(restored.message).toBe('test')
+  it('fromJSON round-trips code and data', () => {
+    const e = new Errors.NotFoundError('test')
+    const restored = e.fromJSON(e.toJSON())
+    expect(restored.caller).toBe('')
+    expect(restored.code).toBe('NotFoundError')
+    expect(restored.data).toEqual({ what: 'test' })
   })
 })
