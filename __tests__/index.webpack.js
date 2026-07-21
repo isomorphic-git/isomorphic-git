3326236814cf6b109c4a528c8434cfba218e90ff
Jakub Jankiewicz <jcubic@jcubic.pl>
2026-07-21 19:24:26 +0200

    fix build

--- __tests__/index.webpack.js
+++ __tests__/index.webpack.js
@@ -4,6 +4,7 @@ import './__helpers__/jasmine-inline-snapshots.js'
 // Import all "test-*.js" modules in this directory (but not subdirectories).
 // `import.meta.webpackContext` is the webpack 5 ESM-native equivalent of the
 // old `require.context`, which cannot be used from an ES module.
+// @ts-ignore - `webpackContext` is a webpack-specific extension of `import.meta`
 const testsContext = import.meta.webpackContext('.', {
   recursive: false,
   regExp: /\/test-.*$/,
