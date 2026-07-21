90ea101d329daa84b99cc0140a6275896ebbaf68
Rayan Abdul Cader <141821420+minutechreview@users.noreply.github.com>
2026-07-21 20:03:47 +0300

    fix(statusMatrix): do not traverse symlinks in GitWalkerFs (#1215) (#2382)
    * fix(statusMatrix): do not traverse symlinks in GitWalkerFs (#1215)
    
    * test: move symlink statusMatrix test to server-only suite
    
    * style: lint symlink statusMatrix test

---
+++ __tests__/server-only.test-statusMatrix-symlink.js
@@ -0,0 +1,25 @@
+/* eslint-env node, jasmine */
+import * as path from 'path'
+
+import { statusMatrix } from 'isomorphic-git'
+
+import { makeFixture } from './__helpers__/FixtureFS.js'
+
+describe('statusMatrix with symlink', () => {
+  it('statusMatrix with symlink targeting ignored directory', async () => {
+    const { fs, dir, gitdir, _fs } = await makeFixture('test-empty')
+    await fs.write(path.join(dir, '.gitignore'), 'ignored-dir\n')
+    await fs.mkdir(path.join(dir, 'ignored-dir'))
+    await fs.write(path.join(dir, 'ignored-dir', 'file.txt'), 'secret')
+    await _fs.promises.symlink(
+      'ignored-dir',
+      path.join(dir, 'symlink-to-ignored')
+    )
+
+    const matrix = await statusMatrix({ fs, dir, gitdir })
+    expect(matrix).toEqual([
+      ['.gitignore', 0, 2, 0],
+      ['symlink-to-ignored', 0, 2, 0],
+    ])
+  })
+})
