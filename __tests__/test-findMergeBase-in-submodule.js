cc3459ac4108d54f99e29411707efaa61aca1fde
Luan Taraschi <130802253+luantaraschi@users.noreply.github.com>
2026-08-13 09:37:07 -0300

    fix: find best merge bases in DAGs

--- __tests__/test-findMergeBase-in-submodule.js
+++ __tests__/test-findMergeBase-in-submodule.js
@@ -1,5 +1,5 @@
 /* eslint-env node, browser, jasmine */
-import { findMergeBase } from 'isomorphic-git'
+import { findMergeBase, writeCommit, writeTree } from 'isomorphic-git'
 
 import { makeFixtureAsSubmodule } from './__helpers__/FixtureFSSubmodule.js'
 
@@ -87,6 +87,33 @@ describe('findMergeBase', () => {
       ],
     })
     expect(base).toEqual(['f79577b91d302d87e310c8b5af8c274bbf45502f'])
+  })
+  it('does not prefer a closer but older common ancestor', async () => {
+    const { fs, gitdir } = await makeFixtureAsSubmodule('test-findMergeBase')
+    const tree = await writeTree({ fs, gitdir, tree: [] })
+    const author = {
+      name: 'Test Author',
+      email: 'test@example.com',
+      timestamp: 1502484200,
+      timezoneOffset: 0,
+    }
+    const commit = (message, parent) =>
+      writeCommit({
+        fs,
+        gitdir,
+        commit: { author, committer: author, message, parent, tree },
+      })
+
+    const e = await commit('E', [])
+    const f = await commit('F', [e])
+    const d = await commit('D', [e])
+    const c = await commit('C', [d])
+    const b = await commit('B', [c])
+    const a = await commit('A', [b])
+    const head = await commit('HEAD', [a, f])
+
+    const base = await findMergeBase({ fs, gitdir, oids: [head, c] })
+    expect(base).toEqual([c])
   })
   it('diverging scenarios', async () => {
     // Setup
--- src/commands/findMergeBase.js
+++ src/commands/findMergeBase.js
@@ -17,42 +17,67 @@ export async function _findMergeBase({ fs, cache, gitdir, oids }) {
   // and computing virtual merge bases is just too much for me to fathom right now.
 
   // If we start N independent walkers, one at each of the given `oids`, and walk backwards
-  // through ancestors, eventually we'll discover a commit where each one of these N walkers
-  // has passed through. So we just need to keep track of which walkers have visited each commit
-  // until we find a commit that N distinct walkers has visited.
+  // through ancestors, eventually we'll discover commits where each one of these N walkers
+  // has passed through. We cannot stop at the first common commit because a better common
+  // ancestor may still be present in another branch of the graph.
   const visits = {}
   const passes = oids.length
+  const common = new Set()
+  const parents = new Map()
+  const readParents = async oid => {
+    if (parents.has(oid)) return parents.get(oid)
+    try {
+      const { object } = await readObject({ fs, cache, gitdir, oid })
+      const commit = GitCommit.from(object)
+      const result = commit.parseHeaders().parent
+      parents.set(oid, result)
+      return result
+    } catch (err) {
+      parents.set(oid, [])
+      return []
+    }
+  }
   let heads = oids.map((oid, index) => ({ index, oid }))
   while (heads.length) {
     // Count how many times we've passed each commit
-    const result = new Set()
     for (const { oid, index } of heads) {
       if (!visits[oid]) visits[oid] = new Set()
       visits[oid].add(index)
       if (visits[oid].size === passes) {
-        result.add(oid)
+        common.add(oid)
       }
     }
-    if (result.size > 0) {
-      return [...result]
-    }
-    // We haven't found a common ancestor yet
     const newheads = new Map()
     for (const { oid, index } of heads) {
-      try {
-        const { object } = await readObject({ fs, cache, gitdir, oid })
-        const commit = GitCommit.from(object)
-        const { parent } = commit.parseHeaders()
-        for (const oid of parent) {
-          if (!visits[oid] || !visits[oid].has(index)) {
-            newheads.set(oid + ':' + index, { oid, index })
-          }
+      // Parents of a common commit cannot be better merge bases.
+      if (common.has(oid)) continue
+      for (const parent of await readParents(oid)) {
+        if (!visits[parent] || !visits[parent].has(index)) {
+          newheads.set(parent + ':' + index, { oid: parent, index })
         }
-      } catch (err) {
-        // do nothing
       }
     }
     heads = Array.from(newheads.values())
   }
-  return []
+
+  if (common.size < 2) return [...common]
+
+  // A merge base is only "best" when it is not an ancestor of another
+  // common ancestor. This also keeps independent criss-cross bases.
+  const redundant = new Set()
+  for (const oid of common) {
+    const ancestors = [...(await readParents(oid))]
+    const seen = new Set()
+    while (ancestors.length) {
+      const ancestor = ancestors.pop()
+      if (seen.has(ancestor)) continue
+      seen.add(ancestor)
+      if (common.has(ancestor)) {
+        redundant.add(ancestor)
+      } else {
+        ancestors.push(...(await readParents(ancestor)))
+      }
+    }
+  }
+  return [...common].filter(oid => !redundant.has(oid))
 }
